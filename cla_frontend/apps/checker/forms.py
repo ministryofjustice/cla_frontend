dc13d7b12cb64c384399c9c5b1c3bddd70396cf5
Marco Fucci <marcofucci@gmail.com>
2014-02-06 16:08:32 +0000

    more your finances logic

--- cla_frontend/apps/checker/forms.py
+++ cla_frontend/apps/checker/forms.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
-
 from django import forms
 
+from core.forms import MultipleFormsForm
 
 
-class YourFinancesForm(forms.Form):
-    # PROPERTY
-    property_worth = forms.IntegerField(label=u"How much is it worth?", min_value=0)
-    property_morgage_left = forms.IntegerField(
+
+class YourFinancesPropertyForm(forms.Form):
+    worth = forms.IntegerField(label=u"How much is it worth?", min_value=0)
+    morgage_left = forms.IntegerField(
         label=u"How much is left to pay on the mortgage?", min_value=0
     )
-    property_owner = forms.ChoiceField(
+    owner = forms.ChoiceField(
         label=u"Is the property owned by you or is it in joint names?",
         choices=[
             ('Owned by me', 'me'),
@@ -18,7 +18,7 @@ class YourFinancesForm(forms.Form):
         ],
         widget=forms.RadioSelect()
     )
-    property_share = forms.DecimalField(
+    share = forms.DecimalField(
         label=u'What is your share of the property?', decimal_places=2,
         min_value=0, max_value=100
     )
@@ -29,17 +29,26 @@ class YourFinancesForm(forms.Form):
         widget=forms.RadioSelect(choices=((True, 'Yes'), (False, 'No')))
     )
 
-    # SAVINGS
-    savings_bank = forms.IntegerField(
+
+class YourFinancesSavingsForm(forms.Form):
+    bank = forms.IntegerField(
         label=u"Do you have any money saved in a bank or building society?",
         min_value=0
     )
-    savings_investments = forms.IntegerField(
+    investments = forms.IntegerField(
         label=u"Do you have any investments, shares, ISAs?", min_value=0
     )
-    savings_valuable_items = forms.IntegerField(
+    valuable_items = forms.IntegerField(
         label=u"Do you have any valuable items over £££ each?", min_value=0
     )
-    savings_money_owed = forms.IntegerField(
+    money_owed = forms.IntegerField(
         label=u"Do you have any money owed to you?", min_value=0
+    )
+
+
+class YourFinancesForm(MultipleFormsForm):
+    forms_list = (
+        ('property', YourFinancesPropertyForm),
+        ('your_savings', YourFinancesSavingsForm),
+        ('partners_savings', YourFinancesSavingsForm),
     )
---
+++ cla_frontend/apps/core/forms.py
@@ -0,0 +1,51 @@
+from django import forms
+
+
+class MultipleFormsForm(forms.Form):
+    forms_list = ()
+
+    def __init__(self, *args, **kwargs):
+        initial = kwargs.get('initial')
+        self.forms = []
+
+        for prefix, form_class in self.forms_list:
+            form_kwargs = dict(kwargs)
+            form_kwargs.update({
+                'prefix': prefix,
+                'initial': initial.get(prefix, {})
+            })
+            form_kwargs = self.get_form_kwargs(**form_kwargs)
+
+            form = form_class(*args, **form_kwargs)
+            self.forms.append((prefix, form))
+
+        super(MultipleFormsForm, self).__init__(*args, **kwargs)
+
+    def get_form_kwargs(self, **kwargs):
+        return kwargs
+
+    def is_valid(self, *args, **kwargs):
+        return all(
+            [form.is_valid(*args, **kwargs) for prefix, form in self.forms]
+        )
+
+    def get_form_by_prefix(self, prefix):
+        for _prefix, form in self.forms:
+            if _prefix == prefix:
+                return form
+        return None
+
+    @property
+    def errors(self):
+        errors = {}
+
+        for prefix, form in self.forms:
+            if form.errors:
+                errors[prefix] = form.errors
+        return errors
+
+    def __getattr__(self, name):
+        form =  self.get_form_by_prefix(name)
+        if form:
+            return form
+        return super(MultipleFormsForm, self).__getattr__(name)
