32af54d9b230fe8692de6815595407b7d70724ca
Ravi Kotecha <kotecha.ravi@gmail.com>
2014-02-06 15:10:01 +0000

    abstract out y/n radio fields

--- cla_frontend/apps/checker/forms.py
+++ cla_frontend/apps/checker/forms.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
-
 from django import forms
-
+from django.forms.forms import Form
+from django.forms import fields
+from .fields import RadioBooleanField
 
 
 class YourFinancesForm(forms.Form):
@@ -42,4 +43,36 @@ class YourFinancesForm(forms.Form):
     )
     savings_money_owed = forms.IntegerField(
         label=u"Do you have any money owed to you?", min_value=0
+    )
+
+
+class YourDetailsForm(Form):
+    has_partner = RadioBooleanField(required=True,
+                                     label='Do you have a partner?'
+    )
+
+    has_benefits = RadioBooleanField(required=True,
+                                     label='Are you or your partner on any benefits?'
+    )
+
+
+    has_children = RadioBooleanField(required=True,
+                                     label='Do you have children?'
+    )
+
+
+    caring_responsibilities = RadioBooleanField(required=True,
+                                         label='Do you any other caring responsibilities??'
+    )
+
+    own_property = RadioBooleanField(required=True,
+                                         label='Do you or your partner own a property?'
+    )
+
+    risk_homeless = RadioBooleanField(required=True,
+                                         label='Are you or your partner aged 60 or over?'
+    )
+
+    older_than_sixty = RadioBooleanField(required=True,
+                                         label='Do you have a partner?'
     )
--- cla_frontend/apps/checker/views.py
+++ cla_frontend/apps/checker/views.py
@@ -1,17 +1,34 @@
 from django.views.generic import TemplateView, FormView
-
 from .forms import YourFinancesForm
+from django.core.urlresolvers import reverse
+from django.shortcuts import redirect
+from .forms import YourDetailsForm
 
 
 class YourProblemView(TemplateView):
     template_name = 'checker/your_problem.html'
 
+    def post(self, request, *args, **kwargs):
+        response = request.POST.get('response')
+        destination = reverse('checker:your_problem', kwargs={'category':response})
+        return redirect(destination)
 
-class YourDetailsView(TemplateView):
+
+class YourDetailsView(FormView):
+    form_class = YourDetailsForm
     template_name = 'checker/your_details.html'
 
+    def get_success_url(self):
+        return reverse('checker:your_finances')
 
-class YourFinancesView(FormView):
+
+    def get_context_data(self, **kwargs):
+        context = super(YourDetailsView, self).get_context_data(**kwargs)
+        context.update(self.kwargs)
+        return context
+
+
+class YourFinancesView(TemplateView):
     template_name = 'checker/your_finances.html'
     form_class = YourFinancesForm
 
