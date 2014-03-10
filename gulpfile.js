3ecb389d5989953447ff03d94db1baf7d9975686
Dom Smith <dom@teneightfive.com>
2014-03-10 16:05:28 +0000

    Bulk out gulpfile and add basic instructions

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,48 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require('gulp'),
+    plugins = require("gulp-load-plugins")(),
+    stylish = require('jshint-stylish');
 
+var paths = {
+  dest_dir: 'cla_frontend/assets/',
+  styles: 'cla_frontend/assets-src/stylesheets/*.scss',
+  scripts: ['cla_frontend/assets-src/javascripts/cla.main.js'],
+  images: 'cla_frontend/assets-src/images/**/*'
+};
+
+// compile scss
 gulp.task('sass', function() {
-  gulp.src(['./cla_frontend/assets/stylesheets/*.scss'])
-      .pipe(sass({includePaths: ['./cla_frontend/assets/stylesheets'], errLogToConsole: true}))
-      .pipe(gulp.dest('./cla_frontend/static/stylesheets'));
+  return gulp.src(paths.styles)
+              .pipe(plugins.sass({errLogToConsole: true}))
+              .pipe(gulp.dest(paths.dest_dir + 'stylesheets'));
 });
 
+// concat js
+gulp.task('js', function() {
+  return gulp.src(paths.scripts)
+              .pipe(plugins.concat('cla.main.js'))
+              .pipe(gulp.dest(paths.dest_dir + 'javascripts'));
+});
 
+// jshint js code
+gulp.task('lint', function() {
+  return gulp.src(paths.scripts)
+              .pipe(plugins.jshint())
+              .pipe(plugins.jshint.reporter(stylish));
+});
+
+// optimise images
+gulp.task('images', function() {
+  return gulp.src(paths.images)
+              .pipe(plugins.imagemin({optimizationLevel: 5}))
+              .pipe(gulp.dest(paths.dest_dir + 'images'));
+});
+
+// setup watches
+gulp.task('watch', function() {
+  gulp.watch(paths.styles, ['sass']);
+  gulp.watch(paths.scripts, ['js', 'lint']);
+});
+
+// setup default tasks
 gulp.task('default', ['build']);
-gulp.task('build', ['sass']);
+gulp.task('build', ['sass', 'js', 'lint', 'images']);
