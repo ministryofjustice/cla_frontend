068741b752f2aada020ae98e11fc43ac03379e66
Dom Smith <dom@teneightfive.com>
2014-03-13 10:58:33 +0000

    Change phase to alpha

--- cla_frontend/apps/core/context_processors.py
+++ cla_frontend/apps/core/context_processors.py
@@ -2,7 +2,7 @@ def globals(request):
   return {
     'app_title': 'Civil Legal Aid',
     'proposition_title': 'Civil Legal Aid',
-    'phase': 'beta',
+    'phase': 'alpha',
     'product_type': 'service',
     'feedback_url': '#',
     'ga_id': '',
