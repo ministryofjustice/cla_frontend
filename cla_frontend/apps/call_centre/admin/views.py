79d6167c59bd2398c6c97c2eb1c2d1c4e7e3ada1
Marco Fucci <info@marcofucci.com>
2014-05-27 17:22:22 +0100

    tested access to rota admin page

--- cla_frontend/apps/call_centre/admin/views.py
+++ cla_frontend/apps/call_centre/admin/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from cla_auth.utils import call_centre_zone_required, manager_member_required
 
+
 @call_centre_zone_required
 @manager_member_required
 def rota(request):
