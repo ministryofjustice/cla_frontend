59fb1288fe248dbc386af01fdd47a3480ec63a7b
Si Parker <si.parker@digital.justice.gov.uk>
2014-06-06 14:14:02 +0100

    added: custom date filter

--- cla_frontend/assets-src/javascripts/app/js/filters.js
+++ cla_frontend/assets-src/javascripts/app/js/filters.js
@@ -1,5 +1,12 @@
 'use strict';
 (function(){
 
-})();
+	angular.module('cla.filters', []).filter('general_date', [ '$filter', function(filter) {
+		// builtin $filter inheritance to create a standardised date filter
+		var builtInDateFilter = filter('date');
+		return function(date_str) {
+		    return builtInDateFilter(date_str, 'short');
+		};
+	}]);
 
+})();
