9e9ec0d33ee284585d2dfe32101175caefeaa0e4
Ravi Kotecha <kotecha.ravi@gmail.com>
2014-06-06 10:25:36 +0100

    don't pollute global scope

--- cla_frontend/assets-src/javascripts/app/js/app.js
+++ cla_frontend/assets-src/javascripts/app/js/app.js
@@ -1,18 +1,24 @@
 'use strict';
+(function(){
 // APP
-angular.module('cla.app', ['djangoRESTResources', 'ui.router',
-  'cla.controllers', 'cla.services', 'cla.filters', 'cla.directives',
-  'cla.states'])
-  .run(function ($rootScope, $state, $stateParams) {
-    $rootScope.$state = $state;
-    $rootScope.$stateParams = $stateParams;
-  });
-angular.module('cla.controllers',[]);
-angular.module('cla.services',[]);
-angular.module('cla.filters',[]);
-angular.module('cla.directives',[]);
-angular.module('cla.states',[]);
-
-
-
-
+  angular.module('cla.app',
+    [
+      'djangoRESTResources',
+      'ui.router',
+      'cla.controllers',
+      'cla.services',
+      'cla.filters',
+      'cla.directives',
+      'cla.states',
+      'cla.utils'])
+    .run(function ($rootScope, $state, $stateParams) {
+      $rootScope.$state = $state;
+      $rootScope.$stateParams = $stateParams;
+    });
+  angular.module('cla.controllers',[]);
+  angular.module('cla.services',[]);
+  angular.module('cla.filters',[]);
+  angular.module('cla.directives',[]);
+  angular.module('cla.states',[]);
+  angular.module('cla.utils',[]);
+})();
--- cla_frontend/assets-src/javascripts/app/js/states.js
+++ cla_frontend/assets-src/javascripts/app/js/states.js
@@ -1,50 +1,58 @@
 'use strict';
+(function() {
 // STATES
-var states = angular.module('cla.states');
-states.CaseListState = {
-  url: '/operator/?search?sort',
-  templateUrl: '/static/javascripts/app/partials/case_list.html',
-  controller: 'CaseListCtrl'
-};
+  var states = angular.module('cla.states');
+  states.Layout = {
+    abstract: true,
+    templateUrl: '/static/javascripts/app/partials/base.html'
+  };
 
-states.CaseDetailState = {
-  abstract: true,
-  url: '/operator/:caseref/',
-  templateUrl: '/static/javascripts/app/partials/case_detail.html',
-  controller: ['$scope', 'case', function($scope, $case){
-    $scope.case = $case;
-  }],
-  resolve: {
-    'case': ['Case', 'EligibilityCheck', '$stateParams', function(Case, EligibilityCheck, $stateParams) {
-      return Case.get({caseref: $stateParams.caseref});
-    }]
-  }
-};
+  states.CaseListState = {
+    parent: states.Layout,
+    url: '/operator/?search?sort',
+    templateUrl: '/static/javascripts/app/partials/case_list.html',
+    controller: 'CaseListCtrl'
+  };
 
-
-states.CaseEditDetailState = {
-  url: '',
-  views: {
-    '': {
-      templateUrl: '/static/javascripts/app/partials/case_detail.edit.html',
-      controller: ['$scope', 'EligibilityCheck', function($scope, EligibilityCheck){
-        console.log($scope.eligibility_check);
-        $scope.submit = function(){
-          $scope.case.$case_details_patch();
-        };
-      }]
-    },
-    'outcome': {
-      templateUrl: '/static/javascripts/app/partials/case_detail.outcome.html',
-      controller: ['$scope', function($scope){console.log('edit.outcome state loaded');}]
-    },
-    'personalDetails': {
-      templateUrl: '/static/javascripts/app/partials/case_detail.personal_details.html',
-      controller: ['$scope', function($scope){
-        $scope.submit = function(){
-          $scope.case.$personal_details_patch();
-        };
+  states.CaseDetailState = {
+    parent: states.Layout,
+    abstract: true,
+    url: '/operator/:caseref/',
+    templateUrl: '/static/javascripts/app/partials/case_detail.html',
+    controller: ['$scope', 'case', function($scope, $case){
+      $scope.case = $case;
+    }],
+    resolve: {
+      'case': ['Case', 'EligibilityCheck', '$stateParams', function(Case, EligibilityCheck, $stateParams) {
+        return Case.get({caseref: $stateParams.caseref});
       }]
     }
-  }
-};
+  };
+
+  states.CaseEditDetailState = {
+    url: '',
+    views: {
+      '': {
+        templateUrl: '/static/javascripts/app/partials/case_detail.edit.html',
+        controller: ['$scope', 'EligibilityCheck', function($scope, EligibilityCheck){
+          console.log($scope.eligibility_check);
+          $scope.submit = function(){
+            $scope.case.$case_details_patch();
+          };
+        }]
+      },
+      'outcome': {
+        templateUrl: '/static/javascripts/app/partials/case_detail.outcome.html',
+        controller: ['$scope', function($scope){console.log('edit.outcome state loaded');}]
+      },
+      'personalDetails': {
+        templateUrl: '/static/javascripts/app/partials/case_detail.personal_details.html',
+        controller: ['$scope', function($scope){
+          $scope.submit = function(){
+            $scope.case.$personal_details_patch();
+          };
+        }]
+      }
+    }
+  };
+})();
