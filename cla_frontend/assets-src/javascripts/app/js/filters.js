c0064badcc7d45525e77da1d181cdaf753d4489a
Dom Smith <dom@teneightfive.com>
2014-06-11 17:12:56 +0100

    Add edit/view states for personal details card

--- cla_frontend/assets-src/javascripts/app/js/filters.js
+++ cla_frontend/assets-src/javascripts/app/js/filters.js
@@ -1,12 +1,21 @@
 'use strict';
 (function(){
 
-	angular.module('cla.filters', []).filter('general_date', [ '$filter', function(filter) {
-		// builtin $filter inheritance to create a standardised date filter
-		var builtInDateFilter = filter('date');
-		return function(date_str) {
-      return builtInDateFilter(date_str, 'short');
-		};
-  }]);
+  angular.module('cla.filters', [])
+    .filter('general_date', [ '$filter', function(filter) {
+      // builtin $filter inheritance to create a standardised date filter
+      var builtInDateFilter = filter('date');
+      return function(date_str) {
+        return builtInDateFilter(date_str, 'short');
+      };
+    }])
+
+    .filter('nl2br', function(){
+      return function(text) {
+        if (text === undefined)
+          return;
+        return text.replace(/\n/g, '<br/>');
+      };
+    });
 
 })();
