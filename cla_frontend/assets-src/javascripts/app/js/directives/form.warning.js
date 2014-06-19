fb2fda87b3d2151811a3625ae0a54b227755bd66
Ravi Kotecha <kotecha.ravi@gmail.com>
2014-06-19 14:50:17 +0100

    show warnings

--- cla_frontend/assets-src/javascripts/app/js/directives/form.warning.js
+++ cla_frontend/assets-src/javascripts/app/js/directives/form.warning.js
@@ -1,15 +1,23 @@
 'use strict';
 (function(){
   angular.module('cla.directives')
-    .directive('claServerWarning', ['$parse', function($parse) {
+    .directive('claServerValidate', [function() {
       return {
         restrict: 'E',
         scope: {
           for: '@for'
         },
         link: function ($scope) {
-          $scope.$parent.$watch('warnings.'+$scope.for, function (newVal) {
+          var offWarningWatch, offErrorWatch;
+          offWarningWatch = $scope.$parent.$watch('warnings.' + $scope.for, function (newVal) {
             $scope.warnings_for = newVal;
+          });
+          offErrorWatch = $scope.$parent.$watch('errors.' + $scope.for, function (newVal) {
+            $scope.errors_for = newVal;
+          });
+          $scope.$on('$destroy', function () {
+            offWarningWatch();
+            offErrorWatch();
           });
         },
         templateUrl: '/static/javascripts/app/partials/directives/form.warning.html'
