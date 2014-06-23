403a2186cc886464679e099b0680445556059303
Marco Fucci <info@marcofucci.com>
2014-06-23 16:51:37 +0100

    layout changed in case of incomplete means test

--- cla_frontend/assets-src/javascripts/app/js/controllers.js
+++ cla_frontend/assets-src/javascripts/app/js/controllers.js
@@ -72,10 +72,11 @@
       }]);
 
   angular.module('cla.controllers')
-  .controller('CaseDetailCtrl', ['$scope', 'case', 'eligibility_check',
-      function($scope, $case, $eligibility_check){
+  .controller('CaseDetailCtrl', ['$scope', 'case', 'eligibility_check', 'personal_details',
+      function($scope, $case, $eligibility_check, $personal_details){
         $scope.case = $case;
         $scope.eligibility_check = $eligibility_check;
+        $scope.personal_details = $personal_details;
       }]);
 
   angular.module('cla.controllers')
