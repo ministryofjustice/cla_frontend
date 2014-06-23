1fa34423a563e73d4d9654814a5cceb0be700bc2
Marco Fucci <info@marcofucci.com>
2014-06-23 14:04:54 +0100

    changed case detail edit layout, moved reference and actions to the right, added eligibility title to the main content at the top

--- cla_frontend/assets-src/javascripts/app/js/controllers.js
+++ cla_frontend/assets-src/javascripts/app/js/controllers.js
@@ -72,9 +72,10 @@
       }]);
 
   angular.module('cla.controllers')
-  .controller('CaseDetailCtrl', ['$scope', 'case',
-      function($scope, $case){
+  .controller('CaseDetailCtrl', ['$scope', 'case', 'eligibility_check',
+      function($scope, $case, $eligibility_check){
         $scope.case = $case;
+        $scope.eligibility_check = $eligibility_check;
       }]);
 
   angular.module('cla.controllers')
@@ -150,11 +151,9 @@
 
   angular.module('cla.controllers')
     .controller('EligibilityCheckCtrl',
-      ['$scope', 'Category', 'eligibility_check', 'form_utils',
-      function($scope, Category, eligibility_check, form_utils){
+      ['$scope', 'Category', 'form_utils',
+      function($scope, Category, form_utils){
         $scope.category_list = Category.query();
-
-        $scope.eligibility_check = eligibility_check;
 
         $scope.warnings = {};
 
