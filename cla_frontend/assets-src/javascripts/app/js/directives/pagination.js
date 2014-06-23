f4cc946088929c3b130f577ef561aecd0c51537c
Marco Fucci <info@marcofucci.com>
2014-06-23 11:48:24 +0100

    moved pagination.html partials to includes folder

--- cla_frontend/assets-src/javascripts/app/js/directives/pagination.js
+++ cla_frontend/assets-src/javascripts/app/js/directives/pagination.js
@@ -151,7 +151,7 @@
 
             return {
                 restrict: 'AE',
-                templateUrl:  '/static/javascripts/app/partials/pagination.html',
+                templateUrl:  '/static/javascripts/app/partials/includes/pagination.html',
                 scope: {
                     maxSize: '=?',
                     onPageChange: '&?'
