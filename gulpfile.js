6f2b9d97cc1cc7a890be4d5981c0395b4641586e
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2014-06-25 15:39:11 +0100

    Reworked gulpfile.js to use smaller composable tasks, and added ngmin/closure compilation

--- gulpfile.js
+++ gulpfile.js
@@ -2,123 +2,188 @@ var gulp = require('gulp'),
   plugins = require('gulp-load-plugins')(),
   stylish = require('jshint-stylish'),
   runSequence = require('run-sequence'),
-  angularTemplates = require('gulp-angular-templates');
+  ngmin = require('gulp-ngmin');
 
 var paths = {
+  build_dir: 'cla_frontend/build/',
   dest_dir: 'cla_frontend/assets/',
-  templates_tmp_dest_dir: 'cla_frontend/templates-tmp/',
   src_dir: 'cla_frontend/assets-src/',
   styles: 'cla_frontend/assets-src/stylesheets/**/*.scss',
   partials: 'cla_frontend/assets-src/javascripts/app/partials/**/*.html',
-  scripts: [
-    // vendor scripts
-    'cla_frontend/assets-src/vendor/lodash/dist/lodash.min.js',
-    'cla_frontend/assets-src/vendor/jquery-details/jquery.details.js',
-    'cla_frontend/assets-src/vendor/handlebars/handlebars.js',
-    // angular deps
-    'cla_frontend/assets-src/vendor/angular/angular.js',
-    'cla_frontend/assets-src/vendor/angular-sanitize/angular-sanitize.js',
-    'cla_frontend/assets-src/vendor/angular-animate/angular-animate.js',
-    'cla_frontend/assets-src/vendor/angular-resource/angular-resource.js',
-    'cla_frontend/assets-src/vendor/angular-ui-router/release/angular-ui-router.js',
-    'cla_frontend/assets-src/vendor/angular-i18n/angular-locale_en-gb.js',
-    'cla_frontend/assets-src/javascripts/vendor/xeditable.js',
-    'cla_frontend/assets-src/vendor/moment/moment.js',
-    'cla_frontend/assets-src/vendor/angular-moment/angular-moment.js',
-    // angular app code
-    'cla_frontend/assets-src/javascripts/app/js/app.js',
-    'cla_frontend/assets-src/javascripts/app/js/**/*.js',
-    // compiled angular templates
-    'cla_frontend/templates-tmp/**',
-    // CLA
-    'cla_frontend/assets-src/javascripts/moj.Helpers.js',
-    'cla_frontend/assets-src/javascripts/modules/*'
+  scripts: {
+    angular: [
+      'cla_frontend/assets-src/vendor/angular/angular.js',
+      'cla_frontend/assets-src/vendor/angular-sanitize/angular-sanitize.js',
+      'cla_frontend/assets-src/vendor/angular-animate/angular-animate.js',
+      'cla_frontend/assets-src/vendor/angular-resource/angular-resource.js',
+      'cla_frontend/assets-src/vendor/angular-ui-router/release/angular-ui-router.js',
+      'cla_frontend/assets-src/vendor/angular-i18n/angular-locale_en-gb.js',
+      'cla_frontend/assets-src/javascripts/vendor/xeditable.js',
+      'cla_frontend/assets-src/vendor/moment/moment.js',
+      'cla_frontend/assets-src/vendor/angular-moment/angular-moment.js',
+    ],
+    app: [
+      'cla_frontend/assets-src/javascripts/app/js/app.js',
+      'cla_frontend/assets-src/javascripts/app/js/**/*.js',
+    ],
+    vendor: [
+      'cla_frontend/assets-src/vendor/lodash/dist/lodash.min.js',
+      'cla_frontend/assets-src/vendor/jquery-details/jquery.details.js',
+      'cla_frontend/assets-src/vendor/handlebars/handlebars.js',
+    ],
+    vendor_static: [
+      'cla_frontend/assets-src/javascripts/vendor/fullcalendar.min.js',
+      'cla_frontend/assets-src/javascripts/vendor/modernizr.custom.92045.js',
+    ],
+    moj: [
+      'cla_frontend/assets-src/javascripts/moj.Helpers.js',
+      'cla_frontend/assets-src/javascripts/modules/*'
+    ],
+    misc: [
+      'cla_frontend/assets-src/javascripts/misc/*.js'
+    ],
+    debug: [
+      'cla_frontend/assets-src/javascripts/**/*debug*'
+    ]
+  },
+  main_built_scripts: [
+    'cla_frontend/build/javascripts/main/**/*.js'
   ],
-  scripts_misc: [
-    'cla_frontend/assets-src/javascripts/misc/*.js'
-  ],
-  vendor_scripts: 'cla_frontend/assets-src/javascripts/vendor/*',
   images: 'cla_frontend/assets-src/images/**/*'
 };
 
 // clean out assets folder
-gulp.task('clean', function() {
+gulp.task('clean-assets', function() {
   return gulp
     .src(paths.dest_dir, {read: false})
     .pipe(plugins.clean());
 });
 
+gulp.task('clean-build', function() {
+  return gulp
+    .src(paths.build_dir, {read: false})
+    .pipe(plugins.clean());
+});
+
+gulp.task('clean', ['clean-assets', 'clean-build']);
+
 // compile scss
-gulp.task('sass', function() {
-  gulp
+gulp.task('sass-build', function() {
+  return gulp
     .src(paths.styles)
     .pipe(plugins.rubySass({
       loadPath: 'node_modules/govuk_frontend_toolkit/' // add node module toolkit path
     }))
-    .pipe(gulp.dest(paths.dest_dir + 'stylesheets'));
+    .pipe(gulp.dest(paths.build_dir + 'stylesheets'));
 });
 
-// js templates
-gulp.task('templates', function(){
-  return gulp.src(paths.partials)
-    .pipe(angularTemplates({module: 'cla.templates'}))
-    .pipe(gulp.dest(paths.templates_tmp_dest_dir));
-});
-
-gulp.task('js-concat', ['templates'], function(){
-  var prod = paths.scripts.slice(0);
-  console.log(prod);
-
-  // ignore debug files
-  prod.push('!' + paths.src_dir + '**/*debug*');
-  // create concatinated js file
+gulp.task('sass-release', ['sass-build'], function(){
   return gulp
-    .src(prod.concat(paths.templates_tmp_dest_dir))
-    .pipe(plugins.concat('cla.main.js'))
-    .pipe(gulp.dest(paths.dest_dir + 'javascripts'));
+    .src(paths.build_dir + 'stylesheets/**')
+    .pipe(gulp.dest(paths.dest_dir + 'stylesheets/'));
 });
 
-gulp.task('js-vendor', function(){
-  // copy static vendor files
+gulp.task('js-concat-angular', function(){
   return gulp
-    .src(paths.vendor_scripts)
-    .pipe(gulp.dest(paths.dest_dir + 'javascripts/vendor'));
+    .src(paths.scripts.angular)
+    .pipe(plugins.concat('angular.js'))
+    .pipe(gulp.dest(paths.build_dir + 'javascripts/main/'));
 });
 
-gulp.task('js-debug', function(){
-  // create debug js file
+gulp.task('js-concat-ngmin-app', function(){
   return gulp
-    .src(paths.src_dir + 'javascripts/**/*debug*')
+    .src(paths.scripts.app)
+    .pipe(plugins.concat('app.js'))
+    .pipe(plugins.ngmin())
+    .pipe(gulp.dest(paths.build_dir + 'javascripts/main/'));
+});
+
+gulp.task('js-concat-vendor', function(){
+  return gulp
+    .src(paths.scripts.vendor)
+    .pipe(plugins.concat('vendor.js'))
+    .pipe(gulp.dest(paths.build_dir + 'javascripts/main/'));
+});
+
+gulp.task('js-concat-vendor-static', function(){
+  return gulp
+    .src(paths.scripts.vendor_static)
+    .pipe(plugins.concat('vendor_static.js'))
+    .pipe(gulp.dest(paths.build_dir + 'javascripts/main/'));
+});
+
+gulp.task('js-concat-moj', function(){
+  return gulp
+    .src(paths.scripts.moj)
+    .pipe(plugins.concat('moj.js'))
+    .pipe(gulp.dest(paths.build_dir + 'javascripts/main/'));
+});
+
+gulp.task('js-concat-debug', function(){
+  return gulp
+    .src(paths.scripts.debug)
     .pipe(plugins.concat('cla.debug.js'))
+    .pipe(gulp.dest(paths.build_dir + 'javascripts'));
+});
+
+gulp.task('js-copy-misc', function(){
+  return gulp
+    .src(paths.scripts.misc)
+    .pipe(gulp.dest(paths.build_dir + 'javascripts/misc/'));
+});
+
+gulp.task('js-templates', function(){
+  return gulp.src(paths.partials)
+    .pipe(plugins.angularTemplates({module: 'cla.templates'}))
+    .pipe(gulp.dest(paths.build_dir + 'javascripts/main/templates/'));
+});
+
+gulp.task('js-build-main',
+  ['js-concat-angular', 'js-concat-ngmin-app', 'js-concat-vendor',
+   'js-concat-vendor-static', 'js-concat-moj', 'js-concat-debug',
+   'js-copy-misc', 'js-templates']);
+
+gulp.task('js-concat-main', ['js-build-main'], function(){
+  var src_path = paths.main_built_scripts;
+  src_path.push('!' + paths.build_dir + '**/*debug*');
+
+  return gulp.src(src_path)
+    .pipe(plugins.closureCompiler({
+      compilerPath: 'node_modules/closurecompiler/compiler/compiler.jar',
+      fileName: 'cla.main.js',
+      compilerFlags: {
+        language_in: 'ECMASCRIPT5',
+        warning_level: 'QUIET',
+        compilation_level: 'WHITESPACE_ONLY',
+      }
+    }))
+    .pipe(gulp.dest(paths.build_dir + 'javascripts'));
+});
+
+gulp.task('js-release-main', ['js-concat-main'], function(){
+  return gulp
+    .src(paths.build_dir + 'javascripts/cla.main.js')
     .pipe(gulp.dest(paths.dest_dir + 'javascripts/'));
 });
 
-gulp.task('js-standalone', function(){
-  // copy standalone scripts
+gulp.task('js-release-debug', ['js-concat-debug'], function(){
   return gulp
-    .src(paths.scripts_misc)
-    .pipe(gulp.dest(paths.dest_dir + 'javascripts/misc'));
+    .src(paths.build_dir + 'javascripts/cla.debug.js')
+    .pipe(gulp.dest(paths.dest_dir + 'javascripts/'));
 });
 
-// default js task
-gulp.task('js', ['js-concat', 'js-vendor', 'js-debug', 'js-standalone']);
-
-// remove tmp templates dir
-gulp.task('clean_tmp_templates', ['js'], function(){
+gulp.task('js-release-misc', ['js-copy-misc'], function(){
   return gulp
-    .src(paths.templates_tmp_dest_dir, {read: false})
-    .pipe(plugins.clean({force: true}));
+    .src(paths.build_dir + 'javascripts/misc/*')
+    .pipe(gulp.dest(paths.dest_dir + 'javascripts/misc/'));
 });
+
+gulp.task('js-release',
+  ['js-release-main', 'js-release-misc', 'js-release-debug']);
 
 // jshint js code
 gulp.task('lint', function() {
-  var files = paths.scripts.slice(0);
-
-  // files to ignore from linting
-  files.push('!cla_frontend/assets-src/vendor/**');
-  files.push('!cla_frontend/assets-src/javascripts/vendor/**');
-  files.push('!cla_frontend/assets-src/javascripts/app/templates/**');
-  files.push('!' + paths.templates_tmp_dest_dir + '/**');
+  var files = paths.scripts.app.concat(paths.scripts.moj);
 
   gulp
     .src(files)
@@ -136,16 +201,15 @@ gulp.task('images', function() {
 
 // setup watches
 gulp.task('watch', function() {
-  gulp.watch(paths.styles, ['sass']);
-  gulp.watch(paths.src_dir + 'javascripts/**/*', ['lint', 'js']);
+  gulp.watch(paths.styles, ['sass-release']);
+  gulp.watch(paths.src_dir + 'javascripts/**/*', ['lint', 'js-release']);
   gulp.watch(paths.images, ['images']);
 });
 
 // setup default tasks
 gulp.task('default', ['build']);
 
-// run build
-gulp.task('build', function() {
+gulp.task('build', function(){
   runSequence('clean',
-    ['lint', 'templates', 'js', 'images', 'sass', 'clean_tmp_templates']);
+    ['lint', 'js-release', 'sass-release', 'images'])
 });
