0f1b59d5349922c71a64a659fbe5e0fc378f52fa
Dom Smith <dom@teneightfive.com>
2014-07-10 10:58:02 +0100

    Fix JSHint warnings

--- cla_frontend/assets-src/javascripts/app/js/filters.js
+++ cla_frontend/assets-src/javascripts/app/js/filters.js
@@ -33,13 +33,13 @@
     .filter('snakeCaseToHuman', function() {
       return function(input) {
         return input.charAt(0).toUpperCase() + input.substr(1).replace(/_/g, ' ');
-      }
+      };
     })
 
     .filter('camelCaseToHuman', function() {
       return function(input) {
         return input.charAt(0).toUpperCase() + input.substr(1).replace(/[A-Z]/g, ' $&');
-      }
+      };
     });
 
 })();
