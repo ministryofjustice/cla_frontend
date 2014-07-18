f0bbfab9d13a948db1f615f15559f0375e82a2fa
Marco Fucci <info@marcofucci.com>
2014-07-18 15:34:33 +0100

    started using diagnosis api and finished first version of angular template

--- cla_frontend/assets-src/javascripts/app/js/controllers/diagnosis.js
+++ cla_frontend/assets-src/javascripts/app/js/controllers/diagnosis.js
@@ -2,11 +2,26 @@
   'use strict';
   
   angular.module('cla.controllers')
-    .controller('DiagnosisCtrl',
-      ['$scope',
-        function($scope) {
+    .controller('DiagnosisCtrl', 
+      ['$scope', 'Diagnosis',
+        function($scope, Diagnosis) {
+          $scope.diagnosis = new Diagnosis({
+            'case_reference': $scope.case.reference
+          });
 
+          if (!$scope.case.diagnosis) {
+            $scope.diagnosis.$save();
+          } else {
+            $scope.diagnosis.$get();
+          }
+          
+          $scope.save = function() {
+            $scope.diagnosis.$patch({
+              'case_reference': $scope.case.reference
+            });
+          }
         }
+
       ]
     );
 })();
