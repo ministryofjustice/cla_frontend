62d4bcf6c9d9eda3b926f22278a2ade915625c73
Ravi Kotecha <kotecha.ravi@gmail.com>
2014-08-04 18:35:44 +0100

    clean up safe_to_call directive and icons

--- cla_frontend/assets-src/javascripts/app/js/directives/safe_to_contact.js
+++ cla_frontend/assets-src/javascripts/app/js/directives/safe_to_contact.js
@@ -1,7 +1,7 @@
 'use strict';
 (function(){
   angular.module('cla.directives')
-    .directive('safeToContact', ['CONTACT_SAFETY', '_', function(CONTACT_SAFETY, _) {
+    .directive('safeToContact', ['CONTACT_SAFETY', function(CONTACT_SAFETY) {
       return {
         restrict: 'E',
         scope: {
@@ -14,48 +14,28 @@
             'Safe to contact': CONTACT_SAFETY.SAFE,
             'Not safe to call': CONTACT_SAFETY.DONT_CALL,
             'Not safe to leave a message': CONTACT_SAFETY.NO_MESSAGE
-          }, name;
+          };
 
-          function get_value(name) {
-            return lookup[name];
-          }
-
-          function on_change(value) {
-            if (value) {
-              name = _.invert(lookup)[value];
-              scope.setSafe(_.find(scope.options, {name: name}).safe, name);
-            }
-          }
-
-
-          scope.setSafe = function(safe, name) {
-            scope.person.contact_safety = {
-              'safe': safe,
-              'name': name
-            };
-            scope.person.safe_to_contact = get_value(name);
+          scope.setSafe = function(name) {
+            scope.person.safe_to_contact = lookup[name];
             scope.showOpts = false;
           };
 
           scope.iconClass = function () {
-            if (typeof scope.person.contact_safety === 'undefined') {
+            if (typeof scope.person.safe_to_contact === 'undefined') {
               return 'Icon--call';
-            } else if (scope.person.contact_safety.safe === true) {
+            } else if (scope.person.safe_to_contact === CONTACT_SAFETY.SAFE) {
               return 'Icon--call Icon--green';
-            } else if (scope.person.contact_safety.safe === false) {
+            } else if (scope.person.safe_to_contact !== CONTACT_SAFETY.SAFE) {
               return 'Icon--dontcall Icon--red';
             }
           };
 
           scope.options = [
-            {'name': 'Safe to contact', 'safe': true},
-            {'name': 'Not safe to call', 'safe': false},
-            {'name': 'Not safe to leave a message', 'safe': false}
+            {'name': 'Safe to contact', 'value': CONTACT_SAFETY.SAFE},
+            {'name': 'Not safe to call', 'value': CONTACT_SAFETY.DONT_CALL},
+            {'name': 'Not safe to leave a message', 'value': CONTACT_SAFETY.NO_MESSAGE}
           ];
-
-          scope.$watch('person.safe_to_contact', on_change);
-          on_change(scope.person.safe_to_contact);
-
         }
       };
     }]);
