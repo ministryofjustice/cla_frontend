8a50e0d4b28da6e8c8b7837e5e5f60e8c9090376
Dom Smith <dom@teneightfive.com>
2014-08-04 19:28:58 +0100

    Remove console log

--- cla_frontend/assets-src/javascripts/app/js/runblocks/guidance.js
+++ cla_frontend/assets-src/javascripts/app/js/runblocks/guidance.js
@@ -99,7 +99,6 @@
           });
 
           $rootScope.$on('guidance:toggle', function(__, toggle) {
-            console.log(toggle);
             $scope.show_guidance = toggle;
           });
         }
