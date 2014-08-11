59cf3af20417b4c7389d29803841945e10a73123
Marco Fucci <info@marcofucci.com>
2014-08-11 10:40:55 +0100

    moved means test summary to legalaid.views

--- cla_frontend/apps/call_centre/views.py
+++ cla_frontend/apps/call_centre/views.py
@@ -1,34 +1,8 @@
-import json
-
 from django.shortcuts import render
-from django.http import HttpResponse
-
-from api.client import get_connection
 
 from cla_auth.utils import call_centre_zone_required
-
-from legalaid.shortcuts import get_eligibility_or_404
-
-from cla_common.templatetags.means_summary_tags import MeansSummaryFormatter
 
 
 @call_centre_zone_required
 def dashboard(request):
     return render(request, 'call_centre/dashboard.html', {})
-
-
-@call_centre_zone_required
-def case_means_summary(request, case_reference):
-    client = get_connection(request)
-    eligibility_check = get_eligibility_or_404(client, case_reference)
-
-    formatter = MeansSummaryFormatter()
-    output = formatter.format(eligibility_check)
-
-    class SmartEncoder(json.JSONEncoder):
-        def default(self, obj):
-            if hasattr(obj, '__promise__'):
-                return unicode(obj)
-            return json.JSONEncoder.default(self, obj)
-
-    return HttpResponse(json.dumps(output, cls=SmartEncoder), content_type="application/json")
--- cla_frontend/apps/cla_provider/urls.py
+++ cla_frontend/apps/cla_provider/urls.py
@@ -3,11 +3,17 @@ from django.contrib.auth import views as django_views
 from django.core.urlresolvers import reverse_lazy
 
 from cla_auth import views as auth_views
+from cla_auth.utils import zone_required
+
+from legalaid import views as legalaid_views
 
 from . import views
 
 
 urlpatterns = patterns('',
+    url(r'^case/(?P<case_reference>.+)/means_summary/$', 
+        zone_required(legalaid_views.case_means_summary, zone='cla_provider')
+    ),
     url(r'^login/$', auth_views.login, {
         'zone_name': 'cla_provider'
     }, name='login'),
@@ -19,7 +25,6 @@ urlpatterns = patterns('',
 
     url(r'^proxy/(?P<path>.*)', auth_views.backend_proxy_view, name="backend_proxy"),
     url(r'^.*$', views.dashboard, name='dashboard'),
-    
 
     # TODO: to be removed
     url(r'^case/(?P<case_reference>.+)/edit/$', views.edit_case, name='edit_case'),
