bb87eed11c0d5cac1bd5002afaaea96cc35bcc91
Andy Driver <andy.driver@digital.justice.gov.uk>
2014-09-09 15:00:33 +0100

    Only permit socket connection if Django sessionid is valid

--- cla_socketserver/app.js
+++ cla_socketserver/app.js
@@ -3,14 +3,48 @@ var server = http.createServer().listen(8005);
 var io = require('socket.io')(server);
 var cookie = require('cookie');
 var querystring = require('querystring');
+var Promise = require('promise');
 
+
+function validate_sessionid(sessionid) {
+  return new Promise(function (fulfill, reject) {
+    var options = {
+      method: 'HEAD',
+      host: 'localhost',
+      port: '8001',
+      path: '/call_centre/',
+      cookie: 'sessionid=' + sessionid
+    };
+    var request = http.request(options);
+
+    request.on('response', function (response) {
+      if (redirected_to_login(response)) {
+        reject(Error('not authorized'));
+      }
+      fulfill();
+    });
+
+    request.on('error', function (e) {
+      reject(Error('Problem with request: ' + e.message));
+    });
+
+    request.end();
+  });
+}
+
+function redirected_to_login(response) {
+  return 'Location' in response.headers;
+}
 
 io.use(function (socket, next) {
   if (socket.request.headers.cookie) {
     socket.request.cookie = cookie.parse(socket.request.headers.cookie);
-    return next();
+    validate_sessionid(socket.request.cookie.sessionid).then(next, function (e) {
+      next(new Error('not authorized: ' + e.message));
+    });
+  } else {
+    next(new Error('not authorized'));
   }
-  return next(new Error('not authorized'));
 });
 
 io.on('connection', function (socket) {
