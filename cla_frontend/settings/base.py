03d0b2f1dbe432e2719210998099045271836f65
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2014-09-10 14:44:11 +0100

    production file logging now uses logstash format

--- cla_frontend/settings/base.py
+++ cla_frontend/settings/base.py
@@ -191,6 +191,9 @@ LOGGING = {
         'simple': {
             'format': '%(levelname)s %(message)s'
         },
+        'logstash': {
+            '()': 'logstash_formatter.LogstashFormatter'
+        }
     },
     'filters': {
         'require_debug_false': {
@@ -207,7 +210,7 @@ LOGGING = {
             'filename' : '/var/log/wsgi/app.log',
             'maxBytes': 1024*1024*5, # 5 MB
             'backupCount' : 7,
-            'formatter': 'verbose',
+            'formatter': 'logstash',
             'filters': ['require_debug_false'],
         },
         'debug_file':{
