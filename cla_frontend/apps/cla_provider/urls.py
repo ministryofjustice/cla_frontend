8beb11599368797e7626f72784574d11d10cd5e1
Dom Smith <dom@teneightfive.com>
2014-09-12 14:26:49 +0100

    Add legal help form view

--- cla_frontend/apps/cla_provider/urls.py
+++ cla_frontend/apps/cla_provider/urls.py
@@ -3,6 +3,7 @@ from django.contrib.auth import views as django_views
 from django.core.urlresolvers import reverse_lazy
 
 from cla_auth import views as auth_views
+from cla_provider import views as provider_views
 from cla_auth.utils import zone_required
 
 from legalaid import views as legalaid_views
@@ -13,6 +14,9 @@ from . import views
 urlpatterns = patterns('',
     url(r'^case/(?P<case_reference>.+)/means_summary/$', 
         zone_required(legalaid_views.case_means_summary, zone='cla_provider')
+    ),
+    url(r'^case/(?P<case_reference>.+)/legal_help_form/$', 
+        zone_required(provider_views.legal_help_form, zone='cla_provider')
     ),
     url(r'^login/$', auth_views.login, {
         'zone_name': 'cla_provider'
--- cla_frontend/apps/cla_provider/views.py
+++ cla_frontend/apps/cla_provider/views.py
@@ -1,3 +1,5 @@
+import json
+from api.client import get_connection
 from django.shortcuts import render
 
 from cla_auth.utils import cla_provider_zone_required
@@ -6,3 +8,9 @@ from cla_auth.utils import cla_provider_zone_required
 @cla_provider_zone_required
 def dashboard(request):
     return render(request, 'cla_provider/dashboard.html', {})
+
+
+def legal_help_form(request, case_reference):
+    client = get_connection(request)
+    extract = client.case(case_reference).legal_help_form_extract.get()
+    return render(request, 'cla_provider/legal_help_form.jade', json.loads(extract))
