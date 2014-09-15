9987727ea9ed543aba1fd15c2da9cf9ed8304137
Marco Fucci <info@marcofucci.com>
2014-09-15 17:58:14 +0100

    legal help form WIP

--- cla_frontend/apps/cla_provider/views.py
+++ cla_frontend/apps/cla_provider/views.py
@@ -1,4 +1,3 @@
-import json
 from api.client import get_connection
 from django.shortcuts import render
 
@@ -13,4 +12,20 @@ def dashboard(request):
 def legal_help_form(request, case_reference):
     client = get_connection(request)
     extract = client.case(case_reference).legal_help_form_extract.get()
-    return render(request, 'cla_provider/legal_help_form.jade', json.loads(extract))
+
+    ec = extract['eligibility_check']
+    ec.update({
+        'main_property': None,
+        'additional_SMOD_property': None,
+        'additional_non_SMOD_property': None
+    })
+    for prop in ec['property_set']:
+        if prop['main']:
+            ec['main_property'] = prop
+        else:
+            if prop['disputed']:
+                ec['additional_SMOD_property'] = prop
+            else:
+                ec['additional_non_SMOD_property'] = prop
+
+    return render(request, 'cla_provider/legal_help_form.jade', extract)
