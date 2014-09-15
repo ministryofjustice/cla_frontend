ea26303068d53b7e56aa0ae86803bd745c32a3a9
Marco Fucci <info@marcofucci.com>
2014-09-15 09:53:31 +0100

    cleanup

--- cla_frontend/assets-src/javascripts/app/test/e2e/spMultiCases.js
+++ cla_frontend/assets-src/javascripts/app/test/e2e/spMultiCases.js
@@ -13,20 +13,13 @@
     }
 
     var case_ref1,
-        case_ref2,
-        latest_op_case_ref,
-        latest_sp_case_ref;
+        case_ref2;
 
     describe('As Operator', function () {
 
       beforeEach(utils.setUp);
 
       it('should create cases as operator and assign then to a provider', function() {
-        browser.get(CONSTANTS.callcentreBaseUrl);
-        element(by.css('.CaseList tbody td a:first-child')).getText().then(function(case_ref) {
-          latest_op_case_ref = case_ref;
-        });
-
         modelsRecipe.Case.createAndAssign(1).then(function(reference) {
           case_ref1 = reference;
         });
@@ -85,7 +78,8 @@
       });
 
       it('should be able to split a case and assign it to same provider if they can deal with that category of law', function() {
-        var splitForm = element(by.css('.modal-content [name=split_case_frm]')),
+        var splitButton = element(by.cssContainingText('.Button.Button--secondary', 'Split')),
+            splitForm = element(by.css('.modal-content [name=split_case_frm]')),
             assignToProviderButton = splitForm.element(by.cssContainingText('button', 'Assign to'));
 
         // choose housing category
@@ -98,16 +92,15 @@
         // verify outcome code REF-INT created
         expect(element(by.css('.CaseHistory-label:first-child')).getText()).toBe('REF-INT');
 
-        // go to list and verify that new case created
-        // browser.get(CONSTANTS.providerBaseUrl);
-        // element(by.css('.CaseList tbody td a:first-child')).getText().then(function(new_case_ref1) {
-        //   expect(new_case_ref1).not.toBe(case_ref1);
-        //   latest_sp_case_ref = new_case_ref1;
-
-        //   // go to that case and verify that outcome code 'created by specialist' exists
-        //   browser.get('provider/'+new_case_ref1+'/');
-        //   expect(element(by.cssContainingText('.CaseHistory-logItemNotes', 'Case created by Specialist')).isPresent()).toBe(true);
-        // });
+        // try to split again => validation error
+        expect(splitButton.isDisplayed()).toBe(true);
+        splitButton.click();
+        fillInSplitForm(splitForm, 'housing');
+        assignToProviderButton.click();
+        expect(splitForm.isDisplayed()).toBe(true);
+        expect(
+          splitForm.element(by.cssContainingText('.Error-message', 'This Case has already been split')).isDisplayed()
+        ).toBe(true);
       });
 
       it('should be able to split a case and assign it back to an operator', function() {
@@ -129,22 +122,20 @@
         expect(splitForm.isPresent()).toBe(false);
 
         // verify outcome code REF-EXT created
-        expect(element(by.css('.CaseHistory-label:first-child')).getText()).toBe('REF-EXT');
+        expect(element(by.css('.CaseHistory-label:first-child')).getText()).toBe('REF-EXT');      
 
-        // go to list and verify that no new case has been created
-        // browser.get(CONSTANTS.providerBaseUrl);
-        // element(by.css('.CaseList tbody td a:first-child')).getText().then(function(new_case_ref2) {
-        //   expect(new_case_ref2).toBe(latest_sp_case_ref);
-        // });
+        // try to split again => validation error
+        expect(splitButton.isDisplayed()).toBe(true);
+        splitButton.click();
+        fillInSplitForm(splitForm, 'benefits');
+        assignToOperatorButton.click();
+        expect(splitForm.isDisplayed()).toBe(true);
+        expect(
+          splitForm.element(by.cssContainingText('.Error-message', 'This Case has already been split')).isDisplayed()
+        ).toBe(true);
 
-        // // go back to operator case list and verify that new case created
-        // logout();
-        // utils.setUp();
-        // browser.get(CONSTANTS.callcentreBaseUrl);
-        // element(by.css('.CaseList tbody tr:not(.is-rejected) a:first-child')).getText().then(function(case_ref) {
-        //   expect(case_ref).not.toBe(latest_op_case_ref);
-        // });        
-
+        // dismiss the modal
+        splitForm.element(by.cssContainingText('a', 'Cancel')).click();
       });
     });
 
