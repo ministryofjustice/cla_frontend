9499a9f4ee3233121eace4722d0e706da7b3b459
Dom Smith <dom@teneightfive.com>
2014-09-26 15:45:05 +0100

    Add shortcuts for case list and case detail states

--- cla_frontend/assets-src/javascripts/app/js/controllers/layout.js
+++ cla_frontend/assets-src/javascripts/app/js/controllers/layout.js
@@ -3,8 +3,8 @@
 
   angular.module('cla.controllers')
     .controller('LayoutCtrl',
-      ['$rootScope', '$scope', '$window', 'History', 'user',
-        function($rootScope, $scope, $window, History, user){
+      ['$rootScope', '$scope', '$window', 'History', 'user', 'hotkeys',
+        function($rootScope, $scope, $window, History, user, hotkeys){
           var offStateChange = $rootScope.$on('$stateChangeSuccess', function(event, to, toParams, from, fromParams){
             if (from.name === 'case_list') {
               History.caseListStateParams = fromParams;
@@ -17,6 +17,15 @@
           });
 
           $rootScope.user = user;
+
+          hotkeys.add({
+            combo: 'q+c',
+            description: 'Search cases',
+            callback: function(e) {
+              e.preventDefault();
+              $('#search [name="q"]').focus();
+            }
+          });
         }
       ]
     );
