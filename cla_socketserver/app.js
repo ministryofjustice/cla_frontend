5e0c9d719e540b9890f8039f7af98c1e6a26175e
Marco Fucci <info@marcofucci.com>
2014-09-30 16:59:05 +0100

    Send people count to statsd

--- cla_socketserver/app.js
+++ cla_socketserver/app.js
@@ -2,7 +2,12 @@ var http = require('http')
   , server = http.createServer().listen(8005)
   , io = require('socket.io')(server)
   , nsp = io.of('/socket.io')
-  , peopleManager = require('./utils/peopleManager');
+  , peopleManager = require('./utils/peopleManager')
+  , StatsD = require('node-statsd').StatsD
+  , statsd = new StatsD({
+      host: process.env.STATSD_HOST || 'localhost',
+      post: process.env.STATSD_POST || 8125
+    });
 
 
 nsp.on('connection', function (socket) {
@@ -12,10 +17,16 @@ nsp.on('connection', function (socket) {
 
   socket.on('identify', function(username) {
     peopleManager.identify(nsp, socket, username);
+
+    console.log('sending to statsd people count: '+peopleManager.getPeopleCount());
+    statsd.gauge('people_connected', peopleManager.getPeopleCount());
   });
 
   socket.on('disconnect', function () {
     peopleManager.disconnect(nsp, socket);
+
+    console.log('sending to statsd people count: '+peopleManager.getPeopleCount());
+    statsd.gauge('people_connected', peopleManager.getPeopleCount());
   });
 
   socket.on('startViewingCase', function(caseref) {
@@ -26,6 +37,7 @@ nsp.on('connection', function (socket) {
     peopleManager.stopViewingCase(nsp, socket, caseref);
   });
 });
+
 
 // server.addListener('request', function (req, res) {
 //   res.writeHead(200, {'Content-Type': 'application/json'});
