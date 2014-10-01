5456a3abf4f91e9cbc8afa8cbd92eeb0758a453c
Dom Smith <dom@teneightfive.com>
2014-10-01 13:02:26 +0100

    Refactor call centre providers into separate files

--- cla_frontend/assets-src/javascripts/app/js/controllers/call_centre/case_detail_assign_provider.js
+++ cla_frontend/assets-src/javascripts/app/js/controllers/call_centre/case_detail_assign_provider.js
@@ -2,47 +2,6 @@
   'use strict';
 
   angular.module('cla.controllers.operator')
-    .controller('CaseDetailDeclineHelpCtrl',
-      ['$scope', '$modal', '$q', '$state', 'flash',
-        function($scope, $modal, $q, $state, flash){
-          $scope.decline_help = function(notes) {
-            var parentQ = $q.when(true);
-            var modalOpts = {
-              templateUrl: 'case_detail.outcome_modal.html',
-              controller: 'OutcomesModalCtl',
-              resolve: {
-                tplVars: function() {
-                  return {
-                    'title': 'Decline Help'
-                  };
-                },
-                case: function() { return $scope.case; },
-                event_key: function() { return 'decline_help'; },  //this is also the function name on Case model
-                notes: function() { return notes || ''; }
-              }
-            };
-            var onSuccess = function (result) {
-              if (result) {
-                flash('success', 'Declined help for Case ' + $scope.case.reference);
-              } else {
-                flash('error', 'There was a problem declining help on this case');
-              }
-              $state.go('case_list');
-            };
-
-            if ($scope.$parent && $scope.$parent.decline_help) {
-              parentQ = $scope.$parent.decline_help();
-            }
-
-            parentQ.then(function () {
-              $modal.open(modalOpts).result.then(onSuccess);
-            });
-          };
-        }
-      ]
-    );
-
-  angular.module('cla.controllers.operator')
     .controller('CaseDetailAssignProviderCtrl',
       ['$rootScope', '$scope', '$modal', '$state',
         function($rootScope, $scope, $modal, $state){
@@ -119,41 +78,4 @@
         }
       ]
     );
-
-  angular.module('cla.controllers.operator')
-    .controller('InvalidCtrl',
-    ['$scope', '$modalInstance',
-      function ($scope, $modalInstance) {
-        $scope.close = function () {
-          $modalInstance.dismiss('cancel');
-        };
-
-        $scope.proceed = function() {
-          $modalInstance.close();
-          $scope.case.warned = true;
-          $scope.assign_to_provider();
-        };
-      }
-    ]
-  );
-
-  angular.module('cla.controllers.operator')
-    .controller('CaseDetailValidateAltHelpCtrl',
-      ['$scope', '$modal', '$state',
-        function($scope, $modal, $state){
-          $scope.validateAltHelp = function() {
-
-            if (!$scope.diagnosis || !$scope.diagnosis.category) {
-              $modal.open({
-                templateUrl: 'case_detail.alt_help_modal.html',
-                controller: 'InvalidCtrl'
-              });
-            } else {
-              $state.go('case_detail.alternative_help');
-            }
-          };
-        }
-      ]
-    );
-
 })();
--- cla_frontend/assets-src/javascripts/app/js/controllers/case_detail.js
+++ cla_frontend/assets-src/javascripts/app/js/controllers/case_detail.js
@@ -12,7 +12,6 @@
           $scope.diagnosis = $diagnosis;
           $scope.personal_details = $personal_details;
 
-
           // checking the time after the template as been rendered
           $scope.$evalAsync(function() {
             $rootScope.$emit('timer:check');
@@ -87,104 +86,4 @@
         }
       ]
     );
-
-  angular.module('cla.controllers')
-    .controller('LogListCtrl',
-    ['$scope', '$modal',
-      function ($scope, $modal) {
-        $scope.logSet = [];
-
-        $scope.$watch('log_set.data', function(newVal) {
-          // log set grouping
-          var currentTimer = null;
-          $scope.logSet = [];
-          angular.forEach(newVal, function(log) {
-            if (!log.timer) {
-              $scope.logSet.push([log]);
-            } else {
-              if (log.timer !== currentTimer) {
-                currentTimer = log.timer;
-                $scope.logSet.push([log]);
-              } else {
-                var ll = $scope.logSet[$scope.logSet.length-1];
-                ll.push(log);
-                $scope.logSet[$scope.logSet.length-1] = ll;
-              }
-            }
-          });
-        });
-
-        $scope.showDiagnosisSummary = function(log) {
-          $modal.open({
-            templateUrl: 'includes/diagnosis.summary.modal.html',
-            controller: ['$scope', '$modalInstance', 'log', 'Diagnosis',
-              function($scope, $modalInstance, log, Diagnosis) {
-                $scope.diagnosis = new Diagnosis(log.patch);
-                $scope.diagnosisTitle = function () {
-                  return $scope.diagnosis.isInScopeTrue() ? 'In scope' : 'Not in scope';
-                };
-                $scope.diagnosisTitleClass = function () {
-                  return $scope.diagnosis.isInScopeTrue() ? 'Icon Icon--lrg Icon--solidTick Icon--green' : 'Icon Icon--lrg Icon--solidCross Icon--red';
-                };
-                $scope.close = function () {
-                  $modalInstance.dismiss('cancel');
-                };
-              }
-            ],
-            resolve: {
-              'log': function () {
-                return log;
-              }
-            }
-          });
-        };
-      }
-    ]
-  );
-
-  angular.module('cla.controllers')
-    .controller('SetMatterTypeCtrl',
-    ['$scope', '$modalInstance', 'matter_types', '$state',
-      function ($scope, $modalInstance, matter_types, $state) {
-        $scope.matter_types = matter_types;
-
-        $scope.cancel = function () {
-          $modalInstance.dismiss('cancel');
-          $scope.case.matter_type1 = null;
-          $scope.case.matter_type2 = null;
-        };
-
-        $scope.save = function() {
-          $scope.case.$set_matter_types().then(function () {
-            $modalInstance.close();
-            if ($scope.next) {
-              $state.go($scope.next);
-            }
-          });
-
-        };
-      }
-    ]
-  );
-
-
-  angular.module('cla.controllers')
-    .controller('SetECFundCtrl',
-    ['$scope', '$modalInstance', 'ECF_STATEMENT',
-      function ($scope, $modalInstance, ECF_STATEMENT) {
-        $scope.ecf_statements = ECF_STATEMENT;
-
-        $scope.cancel = function () {
-          $modalInstance.dismiss('cancel');
-        };
-
-        $scope.save = function() {
-          $scope.case.$patch().then(function () {
-            $modalInstance.close();
-          });
-
-        };
-      }
-    ]
-  );
 })();
