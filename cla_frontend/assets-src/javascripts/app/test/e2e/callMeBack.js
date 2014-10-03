7a5df775e6dd06ae3cd0f3731f8669a46c2d479d
Marco Fucci <info@marcofucci.com>
2014-10-03 17:54:27 +0100

    Add e2e tests for CB1

---
+++ cla_frontend/assets-src/javascripts/app/test/e2e/callMeBack.js
@@ -0,0 +1,56 @@
+(function(){
+  'use strict';
+
+  var utils = require('./_utils'),
+      modelsRecipe = require('./_modelsRecipe'),
+      CONSTANTS = require('../protractor.constants');
+
+  describe('As Operator', function() {
+    beforeEach(utils.setUp);
+
+      var cb1CaseRef;
+
+      it('should be able to suspend a case with a CB1 code', function() {
+
+        modelsRecipe.Case.createEmpty().then(function(case_ref) {
+          cb1CaseRef = case_ref;
+
+          browser.get(CONSTANTS.callcentreBaseUrl + case_ref + '/');
+
+          browser.findElement(by.cssContainingText('.CaseDetails-actions button', 'Close')).click();
+
+          var call_me_back_link = by.cssContainingText('.CaseDetails-actions a', 'Callback');
+          expect(browser.isElementPresent(call_me_back_link)).toBe(true);
+          browser.findElement(call_me_back_link).click();
+
+          var modalEl = browser.findElement(by.css('div.modal'));
+
+          // TODO fix this
+          // var browserName;
+          // browser.getCapabilities().then(function(capabilities) {
+          //   browserName = capabilities.caps_.browserName;
+          // });
+
+          // modalEl.findElement(by.css('input[name="datetime"]')).sendKeys(
+          //   browserName === 'firefox' ? '2020-10-03T17:40' : '03-10-2020'
+          // );  // ff likes 2014-10-03T17:40 instead
+          modalEl.findElement(by.css('input[name="datetime"]')).sendKeys('03-10-2020');
+          modalEl.findElement(by.css('textarea[name="notes"]')).sendKeys('CB1 scheduled.');
+          modalEl.findElement(by.css('button[type="submit"]')).click();
+          expect(browser.isElementPresent(by.css('div.modal'))).toBe(false);
+
+
+          browser.get(CONSTANTS.callcentreBaseUrl + case_ref + '/');
+          expect(element.all(by.css('.CaseHistory-label:first-child')).get(0).getText()).toBe('CB1');
+        });
+      });
+
+      it('should not show CB1 cases in the case list queue', function() {
+        browser.get(CONSTANTS.callcentreBaseUrl);
+
+        expect(browser.getLocationAbsUrl()).toContain(CONSTANTS.callcentreBaseUrl);
+
+        expect(browser.isElementPresent(by.cssContainingText('.ListTable td a', cb1CaseRef))).toBe(false);
+      });
+  });
+})();
