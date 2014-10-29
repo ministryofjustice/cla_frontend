673988f3ab55e60365114f2eb04463c348774648
Dom Smith <dom@teneightfive.com>
2014-10-29 15:33:08 +0000

    Add e2e tests for Diversity monitoring

---
+++ cla_frontend/assets-src/javascripts/app/test/e2e/diversityMonitoring.js
@@ -0,0 +1,95 @@
+(function () {
+  'use strict';
+
+  var utils = require('./_utils'),
+      modelsRecipe = require('./_modelsRecipe'),
+      CONSTANTS = require('../protractor.constants');
+
+  var caseRef;
+  var diversityTab = element(by.cssContainingText('.Tabs-tabLink', 'Diversity'));
+  var sectionTitle = element(by.css('.FormBlock-label'));
+  var summary = element(by.css('.SummaryBlock'));
+  var nextBtn = element(by.name('diversity-next'));
+  var backBtn = element(by.name('diversity-back'));
+  var saveBtn = element(by.name('diversity-save'));
+
+  describe('diversity', function () {
+    beforeEach(utils.setUp);
+
+    describe('Operator diversity monitoring', function () {
+      it('should show diversity questions if incomplete', function () {
+        modelsRecipe.Case.createWithInScopeAndEligible().then(function (_caseRef) {
+          caseRef = _caseRef;
+          browser.get(CONSTANTS.callcentreBaseUrl + _caseRef + '/');
+          element(by.css('[ui-sref="case_detail.diversity"]')).click();
+          expect(sectionTitle.getText()).toBe('Gender');
+        });
+      });
+
+      it('should complete gender questions', function () {
+        chooseOption('gender', 'Male');
+        nextBtn.click();
+        expect(summary.getText()).toContain('Gender: Male');
+      });
+
+      it('should complete ethnicity questions', function () {
+        chooseOption('ethnicity', 'Chinese');
+        nextBtn.click();
+        expect(summary.getText()).toContain('Ethnic origin: Chinese');
+      });
+
+      it('should be able to navigate back', function () {
+        backBtn.click();
+        expect(sectionTitle.getText()).toBe('Ethnic origin');
+        assertOption('ethnicity', 'Chinese');
+
+        backBtn.click();
+        expect(sectionTitle.getText()).toBe('Gender');
+        assertOption('gender', 'Male');
+      });
+
+      it('should be able to navigate forwards', function () {
+        nextBtn.click();
+        expect(sectionTitle.getText()).toBe('Ethnic origin');
+        assertOption('ethnicity', 'Chinese');
+
+        nextBtn.click();
+        expect(sectionTitle.getText()).toBe('Disabilities');
+      });
+
+      it('should complete disability questions', function () {
+        chooseOption('disability', 'BLI - Blind');
+        nextBtn.click();
+        expect(summary.getText()).toContain('Disabilities: BLI - Blind');
+      });
+
+      it('should complete religion questions', function () {
+        chooseOption('religion', 'Christian');
+        nextBtn.click();
+        expect(summary.getText()).toContain('Religion / belief: Christian');
+      });
+
+      it('should complete sexual_orientation questions', function () {
+        chooseOption('sexual_orientation', 'Bisexual');
+        saveBtn.click();
+        expect(summary.getText()).toContain('The client has completed diversity monitoring.');
+        expect(diversityTab.getAttribute('class')).toContain('Icon--solidTick Icon--green');
+      });
+
+      it('should have saved and no longer display answers', function () {
+        browser.get(CONSTANTS.callcentreBaseUrl + caseRef + '/diversity/');
+        expect(summary.getText()).toContain('The client has completed diversity monitoring.');
+        expect(diversityTab.getAttribute('class')).toContain('Icon--solidTick Icon--green');
+      });
+    });
+  });
+
+  // helpers
+  function chooseOption (name, value) {
+    element(by.css('[type="radio"][name="' + name + '"][value="' + value + '"]')).click();
+  }
+
+  function assertOption (name, value) {
+    expect(element(by.css('[type="radio"][name="' + name + '"][value="' + value + '"]')).getAttribute('checked')).toBeTruthy();
+  }
+})();
