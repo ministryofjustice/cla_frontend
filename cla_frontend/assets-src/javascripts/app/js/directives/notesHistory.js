9488bac77854ad9e47decbf723b1f75649f1c48d
Dom Smith <dom@teneightfive.com>
2014-11-06 16:17:59 +0000

    Tidy UI for case notes history

--- cla_frontend/assets-src/javascripts/app/js/directives/notesHistory.js
+++ cla_frontend/assets-src/javascripts/app/js/directives/notesHistory.js
@@ -4,67 +4,72 @@
   angular.module('cla.directives')
     .directive('notesHistory', ['$modal', function($modal) {
       return {
-        restrict: 'E',
-        transclude: true,
-        templateUrl: 'directives/notesHistory.html',
+        restrict: 'A',
         scope: {
-          case: '=',
-          type: '@type'
+          type: '@historyType'
         },
-        controller: ['$scope', function($scope) {
-          $scope.showNotesHistory = function() {
+        link: function (scope, elem, attrs) {
+          elem.on('click', function() {
+            scope.caseRef = attrs.notesHistory;
+
             $modal.open({
               templateUrl: 'notes.history.modal.html',
-              controller: ['$scope', '$modalInstance', 'NotesHistory', 'dmp',
-                function($modalScope, $modalInstance, NotesHistory, dmp) {
-                  function updatePage() {
-                    NotesHistory.query(
-                      {
-                        case_reference: $scope.case.reference,
-                        type: $scope.type,
-                        page: $modalScope.currentPage,
-                        with_extra: true
-                      }
-                    ).$promise.then(function(data) {
-                      var el,
-                          prevNotes = ' ';
-
-                      $modalScope.notes = data;
-
-                      for (var i=$modalScope.notes.results.length-1; i>=0; i--) {
-                        el = $modalScope.notes.results[i];
-                        el.diffHTML = dmp.createSemanticDiffHtml(prevNotes || ' ', el.type_notes || ' ');
-                        prevNotes = el.type_notes;
-                      }
-
-                      if (data.next !== null) {
-                        $modalScope.notes.results.pop();
-                      }
-                    });
-                  }
-
-                  $modalScope.currentPage = 1;
-
-                  $modalScope.goToNextPage = function() {
-                    $modalScope.currentPage += 1;
-                    updatePage();
-                  };
-
-                  $modalScope.goToPreviousPage = function() {
-                    $modalScope.currentPage -= 1;
-                    updatePage();
-                  };
-                  
-                  $modalScope.close = function () {
-                    $modalInstance.dismiss('cancel');
-                  };
-
-                  updatePage();
-                }
-              ]
+              scope: scope,
+              controller: 'NotesHistoryModalCtl'
             });
-          };
-        }]
+          });
+        }
       };
     }]);
+
+  angular.module('cla.controllers')
+    .controller('NotesHistoryModalCtl',
+      ['$scope', '$modalInstance', 'NotesHistory', 'dmp',
+        function($scope, $modalInstance, NotesHistory, dmp) {
+          function updatePage() {
+            NotesHistory.query(
+              {
+                case_reference: $scope.caseRef,
+                type: $scope.type,
+                page: $scope.currentPage,
+                with_extra: true
+              }
+            ).$promise.then(function(data) {
+              var el,
+                  prevNotes = ' ';
+
+              $scope.notes = data;
+
+              for (var i=$scope.notes.results.length-1; i>=0; i-=1) {
+                el = $scope.notes.results[i];
+                el.diffHTML = dmp.createSemanticDiffHtml(prevNotes || ' ', el.type_notes || ' ');
+                prevNotes = el.type_notes;
+              }
+
+              if (data.next !== null) {
+                $scope.notes.results.pop();
+              }
+            });
+          }
+
+          $scope.currentPage = 1;
+
+          $scope.goToNextPage = function() {
+            $scope.currentPage += 1;
+            updatePage();
+          };
+
+          $scope.goToPreviousPage = function() {
+            $scope.currentPage -= 1;
+            updatePage();
+          };
+
+          $scope.close = function () {
+            $modalInstance.dismiss('cancel');
+          };
+
+          updatePage();
+        }
+      ]
+    );
 }());
