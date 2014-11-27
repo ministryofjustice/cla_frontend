d643b37ec079ab8ac429cb1f256413d2a29c24cc
Dom Smith <dom@teneightfive.com>
2014-11-27 12:36:01 +0000

    Clean out screenshots on gulp build

--- tasks/gulp/clean-pre.js
+++ tasks/gulp/clean-pre.js
@@ -1,6 +1,6 @@
 (function(){
   'use strict';
-  
+
   var gulp = require('gulp');
   var paths = require('./_paths');
   var ignore = require('gulp-ignore');
@@ -9,7 +9,7 @@
   // clean out assets folder
   gulp.task('clean-pre', function() {
     return gulp
-      .src([paths.dest, paths.tmp], {read: false})
+      .src([paths.dest, paths.tmp, 'reports/protractor-e2e/screenshots/'], {read: false})
       .pipe(ignore('node_modules/**'))
       .pipe(rimraf());
   });
