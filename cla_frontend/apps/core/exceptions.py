368443b2392232393d28b077d8915fe810c1f4c1
Marco Fucci <marcofucci@gmail.com>
2014-12-31 16:17:14 +0000

    Delete unused files

--- cla_frontend/apps/core/exceptions.py
+++
@@ -1,8 +0,0 @@
-from django.core.exceptions import ValidationError
-
-
-class RemoteValidationError(ValidationError):
-    """
-    Exception for when remote validation has failed.
-    """
-    pass
