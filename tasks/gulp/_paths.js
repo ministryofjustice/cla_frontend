020163dd9dfa902f3234f04e62d7bd132eb53080
Dom Smith <dom@teneightfive.com>
2015-01-27 12:14:20 +0000

    Add ng-idle to catch expiring sessions

--- tasks/gulp/_paths.js
+++ tasks/gulp/_paths.js
@@ -69,6 +69,7 @@
       paths.src + 'vendor/ng-text-truncate/ng-text-truncate.js',
       paths.src + 'vendor/angular-local-storage/dist/angular-local-storage.js',
       paths.src + 'vendor/angularUtils/src/directives/pagination/dirPagination.js',
+      paths.src + 'vendor/ng-idle/angular-idle.js',
       paths.src + 'javascripts/vendor/diff-match-patch/angular-diff-match-patch.js',
       paths.src + 'javascripts/vendor/diff-match-patch/google-diff-match-patch.js',
       paths.src + 'vendor/papaparse/papaparse.js', // csv upload
