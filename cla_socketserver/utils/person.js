a92abae122b6e13edfd9ec0b7b39ad70f22ea3f5
Josh Rowe <josh@s-block.com>
2015-07-10 11:31:36 +0100

    tabs to spaces

--- cla_socketserver/utils/person.js
+++ cla_socketserver/utils/person.js
@@ -8,70 +8,70 @@ function Person(username, userType) {
 };
 
 Person.prototype.connect = function(socket, appVersion, userType) {
-	if (typeof this.connections[socket.id] === 'undefined') {
-		this.connections[socket.id] = {
-			caseViewed: null,
-			appVersion: appVersion,
+  if (typeof this.connections[socket.id] === 'undefined') {
+    this.connections[socket.id] = {
+      caseViewed: null,
+      appVersion: appVersion,
       userType: userType
-		};
+    };
     socket.join(userType);
-	}
+  }
 };
 
 Person.prototype.disconnect = function(socket) {
-	conn = this.connections[socket.id];
-	if (conn !== 'undefined') {
+  conn = this.connections[socket.id];
+  if (conn !== 'undefined') {
     socket.leave(conn.userType);
-		delete this.connections[socket.id];
-	}
+    delete this.connections[socket.id];
+  }
 };
 
 Person.prototype.getViewedCase = function(socket) {
-	conn = this.connections[socket.id];
-	if (conn !== 'undefined') {
-		return conn.caseViewed;
-	}
+  conn = this.connections[socket.id];
+  if (conn !== 'undefined') {
+    return conn.caseViewed;
+  }
 
-	return null;
-}
+  return null;
+};
 
 Person.prototype.getAllViewedCase = function() {
-	return _.pluck(_.values(this.connections), 'caseViewed');
-}
+  return _.pluck(_.values(this.connections), 'caseViewed');
+};
 
 Person.prototype.ownsSocket = function(socket) {
-	return _.contains(_.keys(this.connections), socket.id);
+  return _.contains(_.keys(this.connections), socket.id);
 };
 
 Person.prototype.canBeDeleted = function() {
-	return _.isEmpty(this.connections);
-}
+  return _.isEmpty(this.connections);
+};
 
 Person.prototype.startViewingCase = function(socket, caseref) {
-	var conn = this.connections[socket.id];
-	if (typeof conn === 'undefined') {
-		console.error('Something\'s wrong, can\'t find socket data');
-		return;
-	}
+  var conn = this.connections[socket.id];
+  if (typeof conn === 'undefined') {
+    console.error('Something\'s wrong, can\'t find socket data');
+    return;
+  }
 
-	conn.caseViewed = caseref;
-	socket.join(caseref);
+  conn.caseViewed = caseref;
+  socket.join(caseref);
 };
 
 Person.prototype.stopViewingCase = function(socket, caseref) {
-	var conn = this.connections[socket.id];
-	if (typeof conn === 'undefined') {
-		console.error('Something\'s wrong, can\'t find socket data');
-		return;
-	}
+  var conn = this.connections[socket.id];
+  if (typeof conn === 'undefined') {
+    console.error('Something\'s wrong, can\'t find socket data');
+    return;
+  }
 
-	socket.leave(conn.caseViewed);
-	conn.caseViewed = null;
+  socket.leave(conn.caseViewed);
+  conn.caseViewed = null;
 };
 
 Person.prototype.getAllAppVersions = function() {
-	return _.pluck(_.values(this.connections), 'appVersion');
-}
+  return _.pluck(_.values(this.connections), 'appVersion');
+};
 
 
 module.exports = Person;
