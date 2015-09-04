9e99089d4f0be639890bd3f4c59f8656a9e6057a
Josh Rowe <josh@s-block.com>
2015-09-04 11:30:22 +0100

    update placeholder in search to say Search complaints when viewing complaints

--- cla_frontend/assets-src/javascripts/app/js/controllers/search.js
+++ cla_frontend/assets-src/javascripts/app/js/controllers/search.js
@@ -6,11 +6,14 @@
       ['$scope', '$state', '$location',
         function($scope, $state, $location) {
           $scope.$on('$locationChangeSuccess', function(){
+            var searchList = ($state.current.name.indexOf('complaint') !== -1)? 'complaints' : 'cases';
+            var $searchElement = angular.element(document.getElementById('case-search'));
+            $searchElement.attr('placeholder', 'Search ' + searchList);
             $scope.search = $location.search().search || '';
           });
 
           $scope.submit = function() {
-            var action = ($state.current.name.substring(0, 10) === 'complaints')? 'complaints_list' : 'case_list';
+            var action = ($state.current.name.indexOf('complaint') !== -1)? 'complaints_list' : 'case_list';
             $state.go(action, {search: $scope.search}, {inherit: false});
           };
         }
