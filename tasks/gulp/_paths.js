462863e432c54ecb7a6e03c8ff6e57c080445f7b
Alistair Laing <aliuk2012@users.noreply.github.com>
2018-03-06 10:20:12 +0000

    fixes 502 error with websockets

--- tasks/gulp/_paths.js
+++ tasks/gulp/_paths.js
@@ -60,7 +60,7 @@
       paths.src + 'vendor/conduitjs/lib/conduit.js',
       paths.src + 'vendor/postal.js/lib/postal.js',
       paths.src + 'vendor/angular-loading-bar/build/loading-bar.js',
-      paths.src + 'vendor/socket.io-client/socket.io.js',
+      paths.src + 'vendor/socket.io-client/dist/socket.io.js',
       paths.src + 'vendor/angular-socket-io/socket.js',
       paths.src + 'vendor/angulartics/src/angulartics.js',
       paths.src + 'vendor/angulartics/src/angulartics-ga.js',
