af9a946dfd78d3772da439921030f715f08799b6
Alistair Laing <aliuk2012@users.noreply.github.com>
2018-05-02 12:17:27 +0100

    switches from livereload to browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@
   var gulp = require('gulp'),
       runSequence = require('run-sequence'),
       requireDir = require('require-dir'),
+      browserSync = require('browser-sync').create(),
       dir = requireDir('./tasks/gulp'); // load tasks from ./tasks/gulp/ folder
 
   // setup default task
@@ -14,5 +15,11 @@
     runSequence('clean-pre',
       ['copy-fonts', 'images', 'copy-vendor-js', 'lint', 'js-lib-compile', 'js-app-compile', 'css-min'],
       'clean-post');
+  });
+
+  gulp.task('browser-sync',['build'], function() {
+    browserSync.init({
+      proxy: "localhost:8001"
+    });
   });
 })();
--- tasks/gulp/watch.js
+++ tasks/gulp/watch.js
@@ -1,20 +1,29 @@
-(function(){
-  'use strict';
+'use strict';
 
-  var gulp = require('gulp');
-  var paths = require('./_paths');
+var gulp = require('gulp');
+var paths = require('./_paths');
+var browserSync = require('browser-sync');
+var argv = require('yargs').argv;
 
-  gulp.task('watch', function() {
-    var lr = require('gulp-livereload');
-    lr.listen();
 
-    gulp.watch(paths.fonts, ['copy-fonts']);
-    gulp.watch(paths.styles.concat(paths.icons), ['sass']);
-    gulp.watch(paths.images, ['images']);
-    gulp.watch(paths.vendor_static, ['copy-vendor-js']);
-    gulp.watch(paths.src + 'javascripts/**/*', ['lint', 'js-concat']);
-    gulp.watch('tests/**/*.js', ['lint']);
-    // watch built files and send reload event
-    gulp.watch(paths.dest + '**/*').on('change', lr.changed);
+// Proxy existing server via brower-sync and serve on localhost:3000
+gulp.task('watch', ['build'], function() {
+  var host = argv.host || argv.h || 'localhost';
+  var port = argv.port || argv.p || 8001;
+
+  browserSync({
+    proxy: host + ':' + port,
+    open: false,
+    port: 8888
   });
-})();
+
+  gulp.watch(paths.fonts, ['copy-fonts']);
+  gulp.watch(paths.styles.concat(paths.icons), ['sass']);
+  gulp.watch(paths.images, ['images']);
+  gulp.watch(paths.vendor_static, ['copy-vendor-js']);
+  gulp.watch(paths.src + 'javascripts/**/*', ['lint', 'js-concat']);
+  gulp.watch('tests/**/*.js', ['lint']);
+  // watch built files and send reload event
+  gulp.watch(paths.dest + '**/*', browserSync.reload);
+
+});
