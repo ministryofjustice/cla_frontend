09638f0d70c91394411fe50b00e7b251832f61aa
Patrick Farrell <patrick.farrell@digital.justice.gov.uk>
2018-12-20 12:47:19 +0000

    Directly import postcode_lookup view

--- cla_frontend/apps/legalaid/urls.py
+++ cla_frontend/apps/legalaid/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, url
 
 from . import views
+from postcode_lookup_views import postcode_lookup
 
 
 urlpatterns = patterns(
     '',
-    url(r'^addressfinder/(?P<path>.*)', views.postcode_lookup,
+    url(r'^addressfinder/(?P<path>.*)', postcode_lookup,
         name="addressfinder_proxy"),
     url(r'^$', views.home, name='home'),
 )
