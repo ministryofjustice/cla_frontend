5429025bb57cf51d36ac5ae7c88b966961f30360
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2019-10-31 14:11:25 +0000

    Make Diversity tab require eligible, in-scope case
    Grey-out the Diversity tab, and redirect the `resolve` step if the check
    is failed (like we already do on the Assign tab)

--- cla_frontend/assets-src/javascripts/app/js/states/call_centre/case_detail.edit.diversity.js
+++ cla_frontend/assets-src/javascripts/app/js/states/call_centre/case_detail.edit.diversity.js
@@ -18,13 +18,20 @@
       },
       resolve: {
         // check that the eligibility check can be accessed
-        CanAccess: ['$q', 'case', function ($q, $case) {
+        CanAccess: ['$q', 'diagnosis', 'eligibility_check', 'case', function ($q, diagnosis, eligibility_check, $case) {
           var deferred = $q.defer();
 
           if (!$case.personal_details) {
             // reject promise and handle in $stateChangeError
             deferred.reject({
               msg: 'You must add the client\'s details before completing the diversity questionnaire.',
+              case: $case.reference,
+              goto: 'case_detail.edit.diagnosis'
+            });
+          } else if (!diagnosis.isInScopeTrue() || !eligibility_check.isEligibilityTrue()) {
+            // reject promise and handle in $stateChangeError
+            deferred.reject({
+              msg: 'The Case must be <strong>in scope</strong> and <strong>eligible</strong> in order to complete the diversity questionnaire.',
               case: $case.reference,
               goto: 'case_detail.edit.diagnosis'
             });
