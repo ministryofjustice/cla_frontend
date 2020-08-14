16810885f0e4ca122afdaeae1c20cf5596f1849e
said-moj <said.shire@digital.justice.gov.uk>
2020-08-14 14:29:32 +0100

    Exempt /status/ping.json from maintenance mode redirect

--- cla_frontend/apps/core/middleware.py
+++ cla_frontend/apps/core/middleware.py
@@ -17,10 +17,11 @@ class Cla401Middleware(object):
 
 class MaintenanceModeMiddleWare(object):
     MAINTENANCE_PATH = "/maintenance"
+    EXEMPT_PATHS = ["/status/ping.json", MAINTENANCE_PATH]
 
     def process_request(self, request):
         maintenance_mode = getattr(settings, "MAINTENANCE_MODE", False)
-        if maintenance_mode and request.path != self.MAINTENANCE_PATH:
+        if maintenance_mode and request.path not in self.EXEMPT_PATHS:
             return redirect(self.MAINTENANCE_PATH)
         if not maintenance_mode and request.path == self.MAINTENANCE_PATH:
             return redirect("/")
