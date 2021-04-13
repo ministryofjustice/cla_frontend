2a9cb046d8929961e0dbe0c60f76464d3d4140ae
Heather Poole <heather.poole@digital.justice.gov.uk>
2021-04-13 15:21:39 +0100

    initial clear/sort out of base and docker settings

--- cla_frontend/settings/docker.py
+++ cla_frontend/settings/docker.py
@@ -1,46 +1,12 @@
 from .base import *
 import os
 
-try:
-    SECRET_KEY = os.environ["SECRET_KEY"]
-except KeyError as e:
-    print(
-        "Secret key not found, using a default key for the docker build step only, "
-        "please set the SECRET_KEY in your environment"
-    )
-    SECRET_KEY = "CHANGE_ME"
-    pass
-
-DEBUG = os.environ.get("DEBUG", "False") == "True"
-
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split(",")
-
-TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (("CLA", "cla-alerts@digital.justice.gov.uk"),)
 
 MANAGERS = ADMINS
 
 HOST_NAME = os.environ.get("HOST_NAME", "localhost")
-
-BACKEND_BASE_URI = os.environ["BACKEND_BASE_URI"]
-
-ZONE_PROFILES = {
-    "call_centre": {
-        "CLIENT_ID": os.environ.get("CALL_CENTRE_CLIENT_ID"),
-        "CLIENT_SECRET": os.environ.get("CALL_CENTRE_SECRET_ID"),
-        "LOGIN_REDIRECT_URL": "call_centre:dashboard",
-        "BASE_URI": "%s/call_centre/api/v1/" % BACKEND_BASE_URI,
-        "AUTHENTICATION_BACKEND": "call_centre.backend.CallCentreBackend",
-    },
-    "cla_provider": {
-        "CLIENT_ID": os.environ.get("CLA_PROVIDER_CLIENT_ID"),
-        "CLIENT_SECRET": os.environ.get("CALL_PROVIDER_SECRET_ID"),
-        "LOGIN_REDIRECT_URL": "cla_provider:dashboard",
-        "BASE_URI": "%s/cla_provider/api/v1/" % BACKEND_BASE_URI,
-        "AUTHENTICATION_BACKEND": "cla_provider.backend.ClaProviderBackend",
-    },
-}
 
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.CachedStaticFilesStorage"
 
