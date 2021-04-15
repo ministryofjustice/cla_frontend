f3e5ed316decc1b298b8d4cebd8b5818d167d23f
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2021-04-15 10:45:41 +0100

    Use socketserver image in all deploy scripts

--- bin/production_deploy.sh
+++ bin/production_deploy.sh
@@ -7,8 +7,11 @@ HELM_DIR="$ROOT/../helm_deploy/cla-frontend/"
 helm upgrade $RELEASE_NAME \
   $HELM_DIR \
   --namespace=${KUBE_ENV_PRODUCTION_NAMESPACE} \
+  --values ${HELM_DIR}/values-production.yaml \
   --set host=$RELEASE_HOST \
   --set secretName=tls-certificate \
   --set image.repository=$ECR_URL_APP \
   --set image.tag=$IMAGE_TAG \
+  --set socketServer.image.repository=$ECR_URL_SOCKET_SERVER \
+  --set socketServer.image.tag=$IMAGE_TAG \
   --install
