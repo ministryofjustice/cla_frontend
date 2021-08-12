73c5ef1d040b626c21ee378d0ea3657ed56676f6
MayowaAderohunmu-Adeniyi <mayowa.adeniyi@digital.justice.gov.uk>
2021-08-12 13:55:04 +0100

    Pass feature flag variable to our template

--- cla_frontend/apps/call_centre/views.py
+++ cla_frontend/apps/call_centre/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.conf import settings
 
 from cla_auth.utils import call_centre_zone_required
 
 
 @call_centre_zone_required
 def dashboard(request):
-    return render(request, "call_centre/angular_app.html", {})
+    return render(request, "call_centre/angular_app.html", {"family_issue_flag": settings.FAMILY_ISSUE_FEATURE_FLAG})
