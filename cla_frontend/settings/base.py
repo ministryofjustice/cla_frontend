3396ff0c5a32425414dcc309a8ffa40de8e0498d
MayowaAderohunmu-Adeniyi <mayowa.adeniyi@digital.justice.gov.uk>
2021-08-17 09:50:42 +0100

    Set family feature flag as a string instead of boolean to reflect usage
    When using this variable in JS, it has to be wrapped in quotes ie a string. Therefore, having it set as a boolean was confusing since it is never going to be used as one.

--- cla_frontend/settings/base.py
+++ cla_frontend/settings/base.py
@@ -150,7 +150,7 @@ STATICFILES_FINDERS = (
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # Sets whether the updated family issue text displays on in scope family cases or not
-FAMILY_ISSUE_FEATURE_FLAG = os.environ.get("FAMILY_ISSUE_FEATURE_FLAG", "False") == "True"
+FAMILY_ISSUE_FEATURE_FLAG = os.environ.get("FAMILY_ISSUE_FEATURE_FLAG", "False")
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
