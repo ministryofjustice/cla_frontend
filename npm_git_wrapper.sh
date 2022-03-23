56bc6fdcb4510843792edbb88fff1d4310695ccc
Heather Poole <heather.poole@digital.justice.gov.uk>
2022-03-23 15:43:27 +0000

    amended npm_git_wrapper to fix addresses for npm packages

--- npm_git_wrapper.sh
+++ npm_git_wrapper.sh
@@ -1,5 +1,7 @@
 git config --global url."https://github.com/".insteadOf git@github.com:
+git config --global url."https://github.com/".insteadOf git@github.com/
 git config --global url."https://".insteadOf git://
 npm install
 git config --global --unset url."https://github.com/".insteadOf git@github.com:
+git config --global --unset url."https://github.com/".insteadOf git@github.com/
 git config --global --unset url."https://".insteadOf git://
