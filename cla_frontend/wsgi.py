d0e5d0cc4f02d6ec91f829698a700f2b50a0009a
said-moj <said.shire@digital.justice.gov.uk>
2022-04-21 10:07:21 +0100

    Remove uwsgi-prometheus package as the listen queue is exported by default

--- cla_frontend/wsgi.py
+++ cla_frontend/wsgi.py
@@ -19,11 +19,6 @@ from sys import path
 
 from django.core.wsgi import get_wsgi_application
 
-from prometheus_client.core import REGISTRY
-from uwsgi_prometheus.collectors import UWSGIStatsCollector
-
-REGISTRY.register(UWSGIStatsCollector())
-
 SITE_ROOT = dirname(dirname(abspath(__file__)))
 path.append(SITE_ROOT)
 
