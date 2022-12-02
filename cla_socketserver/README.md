f469bebff5fa1b3873823bd357034233b53e4fa4
Michael Clancy <michael.clancy@digital.justice.gov.uk>
2022-12-02 15:01:33 +0000

    LGA-2320: Remove references to statsd data collection in the socket server

--- cla_socketserver/README.md
+++ cla_socketserver/README.md
@@ -24,23 +24,8 @@ message sender). The broadcast message is a list of usernames currently viewing 
 ## Disconnect event
 When the user disconnects from the socket server the `peopleViewing` message is broadcast to all other users still viewing the case.
 
-# Statsd
-When any of of the above messages are received by the socket server, the number of users connected to the socket server as well as 
-the versions of the app that they are using is sent to a statsd server defined in the ` process.env.STATSD_HOST` environment variable.
-
 # Admin app
 The admin app has the following paths:
-
-## /admin
-Redirects to `/admin/broadcast/`, see below.
-
-##/admin/broadcast
-On a `GET` it presents a form with all the users connected and the opportunity to send a message to a specific user or
-all the connected users.
-  
-
-## /admin/peopleMap
-Returns a JSON of all the connected users.
 
 ## /admin/notifications
 Given a message, a broadcast is sent to all connected connected users of a given type (operator or provider).
@@ -48,7 +33,7 @@ Given a message, a broadcast is sent to all connected connected users of a given
 # Access
 The admin web app should not be accessible to the public.
 
-At the moment there is no authentication required to send / receive messages to the socket server. 
+At the moment there is no authentication required to send / receive messages to the socket server.
 
 ## AWS
 The EC2 instances that host the socket server have a security group that limits access to port 8005 to the IP addresses
--- cla_socketserver/app.js
+++ cla_socketserver/app.js
@@ -4,13 +4,7 @@ var app = require('express')()
   , server = require('http').Server(app)
   , bodyParser = require('body-parser')
   , peopleManager = require('./utils/peopleManager')
-  , adminApp = require('./admin')
-  , StatsD = require('node-statsd').StatsD
-  , statsd = new StatsD({
-      host: process.env.STATSD_HOST || 'localhost',
-      post: process.env.STATSD_POST || 8125
-    })
-  , versions = [];
+  , adminApp = require('./admin');
 
 server.listen(8005);
 var io = require('socket.io')(server, {
@@ -36,44 +30,16 @@ app.use(function(err, req, res, next){
 });
 
 // ADMIN
-
 adminApp.install(app, nsp);
 
-
 // SOCKETS
-
-function sendConnStats() {
-  // console.log('sending to statsd people count: '+peopleManager.getPeopleCount());
-  statsd.gauge('people_connected', peopleManager.getPeopleCount());
-
-  var versionCounts = peopleManager.getVersionCounts(),
-      connectedVersions = [];
-
-  _.each(versionCounts, function(value, key) {
-    // storing the versions
-    if (versions.indexOf(key) < 0) { versions.push(key); }
-    if (connectedVersions.indexOf(key) < 0) { connectedVersions.push(key); }
-
-    // console.log('version ' + key + ': ' + value);
-    statsd.gauge('fe_version.'+key, value);
-  });
-
-  // we need to send zeros for versions with no users
-  _.each(_.difference(versions, connectedVersions), function(ver) {
-    // console.log('version ' + ver + ': 0');
-    statsd.gauge('fe_version.'+ver, 0);
-  });
-}
-
 nsp.on('connection', function (socket) {
   socket.on('identify', function(data) {
     peopleManager.identify(nsp, socket, data.username || data, data.usertype, data.appVersion);
-    sendConnStats();
   });
 
   socket.on('disconnect', function () {
     peopleManager.disconnect(nsp, socket);
-    sendConnStats();
   });
 
   socket.on('startViewingCase', function(caseref) {
