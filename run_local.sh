bd4b819286f4f6ff06cca517349640d327f546d9
Jo Gilder <3441835+jogilder@users.noreply.github.com>
2023-01-26 15:07:48 +0000

    LGA-2305 - simple docker environment for frontend. All other environments than cla_frontend are images.

--- run_local.sh
+++ run_local.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 export DOCKER_BUILDKIT=1
 export ENVIRONMENT=${1:-development}
-#export UNIT_TEST=${2:-""}
 echo "running environment $ENVIRONMENT"
 docker-compose down --remove-orphans
-# todo work out what tests need to be run and how by looking at circleci configs
-#if [ $ENVIRONMENT = "test" ]; then
-#  export DJANGO_SETTINGS=cla_backend.settings.circle
-#  docker-compose build cla_frontend --build-arg specific_test_input=$UNIT_TEST
-#  docker-compose run cla_frontend
-#else
-  docker-compose build cla_frontend
-  docker-compose run start_applications
-#   todo work out why this line fails - same thing works in end to end tests
-#  docker-compose exec cla_backend bin/create_db.sh
-#fi
+docker-compose run start_applications
+docker-compose exec clabackend bin/create_db.sh
+
