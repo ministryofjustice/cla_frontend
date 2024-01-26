0d35b2ff77f9d76f973729ae2c99699c484901d8
Ben Millar <ben.millar@digital.justice.gov.uk>
2024-01-26 14:28:09 +0000

    Added run run local using cla_backend

--- run_local.sh
+++ run_local.sh
@@ -1,15 +1,44 @@
 #!/bin/bash
 export DOCKER_BUILDKIT=1
 export ENVIRONMENT=${1:-development}
-echo "running environment $ENVIRONMENT"
-docker-compose down --remove-orphans
-docker-compose up --build
+echo "Running environment: $ENVIRONMENT"
+docker compose down --remove-orphans
 
-# This script is only ran when we are locally testing
-if [ $STATIC_FILES_BACKEND != "s3" ]
+CLA_BACKEND_CID=$(docker ps -q -f status=running -f ancestor=cla_backend-cla_backend)
+
+# If cla_backend_db doesn't exist then we want to start cla_backend
+if [ "$CLA_BACKEND_CID" == "" ];
 then
-    docker-compose exec cla_frontend manage.py collectstatic --noinput
-    
-    # This command is run post install when the application is deployed
-    docker-compose exec cla_backend bin/create_db.sh
+    echo "Starting multi container app"
+    docker compose -f docker-compose.yml up --build -d
+
+    # As we have started the database ourselves we need to run the migrations
+    docker exec cla_backend bin/create_db.sh
+
+    CLA_BACKEND_CID=$(docker ps -q -f status=running -f ancestor=cla_frontend-cla_backend)
+else
+    echo "Starting cla_frontend as standalone"
+    docker compose -f docker-compose-standalone.yml up --build -d
+fi
+
+# cla_backend is not valid hostname so we rename it to clabackend, this allows us to connect to the backend
+docker container rename $CLA_BACKEND_CID clabackend
+
+CLA_FRONTEND_CID=$(docker ps -q -f status=running -f name=cla_frontend-cla_frontend)
+if [ "$CLA_FRONTEND_CID" != "" ];
+then
+    # Collects the frontend js and css assets
+    docker exec $CLA_FRONTEND_CID python manage.py collectstatic --noinput
+
+    # Add cla_frontend to the cla_backend network
+    docker network connect cla_backend_default $CLA_FRONTEND_CID
+else
+    echo "ERROR: Could not find a running cla_frontend container"
+fi
+
+# Removes the exited start applications container
+START_APPLICATIONS_CID=$(docker ps -a -q -f ancestor=jwilder/dockerize)
+if [ "$START_APPLICATIONS_CID" != "" ];
+then
+    docker rm $START_APPLICATIONS_CID
 fi
