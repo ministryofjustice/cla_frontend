ee0032cf19a9519f09591064e3dd527c15d05473
Ben Millar <ben.millar@digital.justice.gov.uk>
2024-01-29 15:02:06 +0000

    Changed to using two run_local scripts

--- run_local.sh
+++ run_local.sh
@@ -1,40 +1,36 @@
 #!/bin/bash
+
+# This runs cla_frontend as a multi-container application
+# This will create the cla_backend containers as defined by the cla_backend application 
+# at ../cla_backend, if you do not have the repository in this location it will be downloaded here.
+
 export DOCKER_BUILDKIT=1
 export ENVIRONMENT=${1:-development}
+export BACKEND_BASE_URI="host.docker.internal:8010"
+
+if [ ! -d "../cla_backend" ]; then
+  echo "cla_backend does not exist at ../cla_backend, clonining it."
+  git clone https://github.com/ministryofjustice/cla_backend ../cla_backend
+fi
+
 echo "Running environment: $ENVIRONMENT"
 docker compose down --remove-orphans
 
-CLA_BACKEND_CID=$(docker ps -q -f status=running -f ancestor=cla_backend-cla_backend)
+echo "Starting multi container app"
+docker compose -f docker-compose.yml up --build -d
 
-# If cla_backend_db doesn't exist then we want to start cla_backend
-if [ "$CLA_BACKEND_CID" == "" ];
-then
-    echo "Starting multi container app"
-    docker compose -f docker-compose.yml up --build -d
-
-    # As we have started the database ourselves we need to run the migrations
-    docker exec cla_backend bin/create_db.sh
-
-    CLA_BACKEND_CID=$(docker ps -q -f status=running -f ancestor=cla_frontend-cla_backend)
-else
-    echo "Starting cla_frontend as standalone"
-    docker compose -f docker-compose-standalone.yml up --build -d
-fi
-
-# cla_backend is not valid hostname so we rename it to clabackend, this allows us to connect to the backend
-docker container rename $CLA_BACKEND_CID clabackend
+# As we have started the database ourselves we need to run the migrations
+docker exec cla_backend bin/create_db.sh
 
 CLA_FRONTEND_CID=$(docker ps -q -f status=running -f name=cla_frontend-cla_frontend)
-if [ "$CLA_FRONTEND_CID" != "" ];
+if [ "$CLA_FRONTEND_CID" == "" ];
 then
-    # Collects the frontend js and css assets
-    docker exec $CLA_FRONTEND_CID python manage.py collectstatic --noinput
-
-    # Add cla_frontend to the cla_backend network
-    docker network connect cla_backend_default $CLA_FRONTEND_CID
-else
     echo "ERROR: Could not find a running cla_frontend container"
+    exit 1
 fi
+
+# Collects the frontend js and css assets
+docker exec $CLA_FRONTEND_CID python manage.py collectstatic --noinput
 
 # Removes the exited start applications container
 START_APPLICATIONS_CID=$(docker ps -a -q -f ancestor=jwilder/dockerize)
