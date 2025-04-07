8a019de339de254085813329d936c40ef1d497cc
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2025-04-07 14:46:10 +0100

    Update service to use ajax to handle feature flags on requests, add in no cache header to ensure no caching on reload

--- cla_frontend/apps/core/views.py
+++ cla_frontend/apps/core/views.py
@@ -1,4 +1,6 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View
+from django.conf import settings
+from django.http import JsonResponse
 
 
 class MaintenanceModeView(TemplateView):
@@ -7,3 +9,13 @@ class MaintenanceModeView(TemplateView):
     def dispatch(self, request, *args, **kwargs):
         context = self.get_context_data(**kwargs)
         return self.render_to_response(context, status=503)
+
+
+class FeatureFlagView(View):
+
+    def dispatch(self, request, *args, **kwargs):
+        flags = {
+            "new_client_note_flag": settings.NEW_CLIENT_NOTE_FEATURE_FLAG,
+        }
+        
+        return JsonResponse(flags)
