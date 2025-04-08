0f310596f171e63aed54c43ca48e245fe5236b9c
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2025-04-08 10:37:48 +0100

    Add trailing slash

--- cla_frontend/apps/core/urls.py
+++ cla_frontend/apps/core/urls.py
@@ -1,4 +1,4 @@
 from django.conf.urls import patterns, url
 from .views import MaintenanceModeView, FeatureFlagView
 
-urlpatterns = patterns("", url(r"^feature-flags$", view=FeatureFlagView.as_view(), name="feature_flags"), url(r"^maintenance$", view=MaintenanceModeView.as_view(), name="maintenance_page"))
+urlpatterns = patterns("", url(r"^feature-flags/$", view=FeatureFlagView.as_view(), name="feature_flags"), url(r"^maintenance$", view=MaintenanceModeView.as_view(), name="maintenance_page"))
