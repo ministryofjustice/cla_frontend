679fc69abadaecd03f0868b334bf919d20f0a17d
Abhi Markan <abhi.markan>
2025-11-19 11:55:27 +0000

    fix(LGA-3818): fixed unit test case duplication

--- tests/angular-js/unit/stripHtmlDirective.spec.js
+++ tests/angular-js/unit/stripHtmlDirective.spec.js
@@ -4,6 +4,50 @@
   describe("Directive: stripHtml", function () {
     var $compile, $rootScope;
 
+    var assertions = [
+      {
+        input: "<b>Test</b> <i>XSS</i>",
+        output: "Test XSS",
+      },
+      {
+        input: "<script>alert('XSS');</script>",
+        output: "",
+      },
+      {
+        input: "<script>alert('XSS');</script>Click",
+        output: "Click",
+      },
+      {
+        input: "<img src=x onerror=alert(1)>Hi",
+        output: "Hi",
+      },
+      {
+        input:
+          "<h1>Heading<h1> <b>Bold</b> <img src=x onerror=alert(1)>Test<object src='data:123' />",
+        output: "Heading Bold Test",
+      },
+      {
+        input: null,
+        output: "",
+      },
+      {
+        input: undefined,
+        output: "",
+      },
+      {
+        input: "Test 1 & 2",
+        output: "Test 1 & 2",
+      },
+      {
+        input: "&lt;script&gt;alert(1)&lt;/script&gt;",
+        output: "<script>alert(1)</script>",
+      },
+      {
+        input: "<scr<script>ipt>alert(document.cookie)</script>",
+        output: "",
+      },
+    ];
+
     // Load directives
     beforeEach(module("ngSanitize"));
     beforeEach(module("cla.directives"));
@@ -23,88 +67,14 @@
       return element;
     }
 
-    it("should remove <b> and <i> HTML tags", function () {
-      // Arrange + Act
-      var element = compileDirective("<b>Test</b> <i>XSS</i>");
+    assertions.forEach(function (assertion) {
+      it("should sanitise the supplied input: " + assertion.input, function () {
+        // Arrange + Act
+        var element = compileDirective(assertion.input);
 
-      // Assert
-      expect(element.text()).toEqual("Test XSS");
-    });
-
-    it("should remove <script> tags", function () {
-      // Arrange + Act
-      var element = compileDirective("<script>alert('XSS');</script>");
-
-      // Assert
-      expect(element.text()).toEqual("");
-    });
-
-    it("should remove <script> tag", function () {
-      // Arrange + Act
-      var element = compileDirective("<script>alert('XSS');</script>Click");
-
-      // Assert
-      expect(element.text()).toEqual("Click");
-    });
-
-    it("should remove <img> tag", function () {
-      // Arrange + Act
-      var element = compileDirective("<img src=x onerror=alert(1)>Hi");
-
-      // Assert
-      expect(element.text()).toEqual("Hi");
-    });
-
-    it("should remove various HTML tags", function () {
-      // Arrange + Act
-      var element = compileDirective(
-        "<h1>Heading<h1> <b>Bold</b> <img src=x onerror=alert(1)>Test<object src='data:123' />"
-      );
-
-      // Assert
-      expect(element.text()).toEqual("Heading Bold Test");
-    });
-
-    it("should handle null safely", function () {
-      // Arrange + Act
-      var element = compileDirective(null);
-
-      // Assert
-      expect(element.text()).toEqual("");
-    });
-
-    it("should handle undefined safely", function () {
-      // Arrange + Act
-      var element = compileDirective(undefined);
-
-      // Assert
-      expect(element.text()).toEqual("");
-    });
-
-    it("should handle plain text safely with ampersand", function () {
-      // Arrange + Act
-      var element = compileDirective("Test 1 & 2");
-
-      // Assert
-      expect(element.text()).toEqual("Test 1 & 2");
-    });
-
-    it("should handle plain text safely with HTML entities", function () {
-      // Arrange + Act
-      var element = compileDirective("&lt;script&gt;alert(1)&lt;/script&gt;");
-
-      // Assert
-      expect(element.text()).toEqual("<script>alert(1)</script>");
-    });
-
-    it("should handle non-recursive filtering", function () {
-      // Arrange + Act
-      var element = compileDirective(
-        "<scr<script>ipt>alert(document.cookie)</script>"
-      );
-
-      // Assert
-      expect(element.text()).toEqual("");
+        // Assert
+        expect(element.text()).toEqual(assertion.output);
+      });
     });
   });
 })();
