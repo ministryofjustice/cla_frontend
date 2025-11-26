38754a8915ef03fa7bdae5a605248c31b6143478
Abhi Markan <abhi.markan@justice.gov.uk>
2025-11-26 08:25:40 +0000

    fix(LGA-3820): session cookie mitigation

--- cla_frontend/apps/cla_auth/urls.py
+++ cla_frontend/apps/cla_auth/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import patterns, url
-from django.core.urlresolvers import reverse_lazy
-
+from cla_auth.views import logout_view
 
 urlpatterns = patterns(
     "",
     url(r"^login/$", "cla_auth.views.login", name="login"),
-    url(r"^logout/$", "django.contrib.auth.views.logout", {"next_page": reverse_lazy("auth:login")}, name="logout"),
+    url(r"^logout/$", logout_view, name="logout"),
 )
