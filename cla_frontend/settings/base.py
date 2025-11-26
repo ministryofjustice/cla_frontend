c320ecd9d78127fe5d507992286900234cfa866e
Abhi Markan <abhi.markan@justice.gov.uk>
2025-11-26 17:05:26 +0000

    fix(LGA-3821): moved to env variable

--- cla_frontend/settings/base.py
+++ cla_frontend/settings/base.py
@@ -311,9 +311,9 @@ LOGIN_URL = "auth:login"
 AUTHENTICATION_BACKENDS = [v["AUTHENTICATION_BACKEND"] for k, v in ZONE_PROFILES.items()]
 
 # Settings for django-session-security.
-SESSION_COOKIE_NAME = os.environ.get("SESSION_COOKIE_NAME", "True") == "__Host-Http-SID"
+SESSION_COOKIE_NAME = os.environ.get("SESSION_COOKIE_NAME", "__Host-Http-SID")
 SESSION_COOKIE_PATH = "/"
-SESSION_COOKIE_SAMESITE = os.environ.get("SESSION_COOKIE_SAMESITE", "True") == "Strict"
+SESSION_COOKIE_SAMESITE = os.environ.get("SESSION_COOKIE_SAMESITE", "Lax")
 SESSION_COOKIE_SECURE = os.environ.get("SESSION_COOKIE_SECURE", "True") == "True"
 SESSION_COOKIE_HTTPONLY = os.environ.get("SESSION_COOKIE_HTTPONLY", "True") == "True"
 
