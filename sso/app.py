df377a5b84896b005e2d0ad9d6b2ecc4449b1b94
said-moj <45761276+said-moj@users.noreply.github.com>
2026-02-04 16:56:09 +0000

    Add obo token generation

--- sso/app.py
+++ sso/app.py
@@ -1,12 +1,12 @@
 from dotenv import load_dotenv
 from flask import Flask, render_template, redirect, url_for, session
 import os
+import msal
 from identity.flask import Auth
 import app_config
 
 
 load_dotenv()
-
 
 def _require_env(name: str) -> str:
     """
@@ -33,7 +33,8 @@ class Config:
     FLASK_SECRET_KEY = _require_env("FLASK_SECRET_KEY")
 
     AUTHORITY = f"https://login.microsoftonline.com/{TENANT_ID}"
-    GRAPH_SCOPES = ["https://graph.microsoft.com/User.Read"]
+    GRAPH_SCOPES = ["https://graph.microsoft.com/.default"]
+    BACKEND_SCOPE =  _require_env("BACKEND_SCOPE")
 
 
 app = Flask(__name__)
@@ -49,6 +50,12 @@ auth = Auth(
     redirect_uri=Config.REDIRECT_URI,
 )
 
+def build_msal_app():
+    return msal.ConfidentialClientApplication(
+        client_id=Config.CLIENT_ID,
+        client_credential=Config.CLIENT_SECRET,
+        authority=Config.AUTHORITY,
+    )
 
 @app.route("/", methods=["GET", "POST"])
 @auth.login_required(scopes=Config.GRAPH_SCOPES)
@@ -56,6 +63,16 @@ def home(*, context):
 
     data = context.get("user")
     name = data.get("name")
+    print("ACCESS TOKEN:", context.get("access_token"))
+    print("GENERATING OBO TOKEN......" )
+    obo_result = build_msal_app().acquire_token_on_behalf_of(
+        user_assertion=context["access_token"],
+        scopes=[Config.BACKEND_SCOPE],
+    )
+    if "error" in obo_result:
+        raise RuntimeError(obo_result["error_description"])
+    else:
+        print("OBO TOKEN:", obo_result)
 
     return render_template("dashboard.html", context=name)
 
