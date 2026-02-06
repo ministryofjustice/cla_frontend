30b2312e5e49d359028b66f88cae63f0c3ffe83b
said-moj <45761276+said-moj@users.noreply.github.com>
2026-02-06 09:31:14 +0000

    Use a dynamic sso image tag

--- bin/uat_deploy.sh
+++ bin/uat_deploy.sh
@@ -20,6 +20,7 @@ helm upgrade $RELEASE_NAME \
   --set ingress.cluster.weight=${INGRESS_CLUSTER_WEIGHT} \
   --set image.repository=$ECR_URL_APP \
   --set image.tag=$IMAGE_TAG \
+  --set image.sso.tag=$SSO_IMAGE_TAG \
   --set socketServer.image.repository=$ECR_URL_SOCKET_SERVER \
   --set socketServer.image.tag=$IMAGE_TAG \
   --set-string sharedIPRangesLAA=$SHARED_IP_RANGES_LAA \
