c0ff5ec34287d01e2f1a6cc97389448d9685134a
said-moj <45761276+said-moj@users.noreply.github.com>
2026-03-09 12:44:30 +0000

    Add roles

--- cla_frontend/apps/cla_auth/backend.py
+++ cla_frontend/apps/cla_auth/backend.py
@@ -22,10 +22,12 @@ logger = logging.getLogger(__name__)
 ROLES = {
     "Civil Legal Advice Helpline Operator Manager": {"ui": "operator", "is_manager": True},
     "Civil Legal Advice Helpline Operator": {"ui": "operator", "is_manager": False},
+    "Civil Legal Advice Helpline Provider": {"ui": "provider", "is_manager": False},
+    # Todo: remove the below roles from silas
     "Civil Legal Advice Operator": {"ui": "operator", "is_manager": False},
     "Civil Legal Advice - Provider": {"ui": "provider", "is_manager": False},
     "Civil Legal Advice Access": {"ui": "provider", "is_manager": False},
-    "Civil Legal Advice Helpline Provider": {"ui": "provider", "is_manager": False},
+    "Civil Legal Advice - Helpline Operator": {"ui": "operator", "is_manager": False},
 }
 
 
